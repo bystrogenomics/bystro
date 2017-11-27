799b9e844df91d358edcda0094bdde0b8280bc97
Alexei Kotlar <akotlar@bu.edu>
2017-11-27 17:22:16 +0000

    Scale all numbers, not just floats; so that a 1 in phastCons becomes 1 * scalingFactor, and so that it can be restored to 1 by / scalingFactor

--- lib/Seq/Tracks/Score/Build/Round.pm
+++ lib/Seq/Tracks/Score/Build/Round.pm
@@ -14,11 +14,6 @@ sub round {
   #my ($self, $value) = @_;
   #   ($_[0], $_[1] ) = @_;
 
-  #If we have an exact figure, return an int, stored by msgpack as an int
-  if( int($_[1]) == $_[1] ) {
-    return int($_[1]);
-  }
-
   #We have updated Data::MessagePack to support, enforce single-precision floats
   #So 5 bytes at most when prefer_float32() enabled
   return lround($_[1] * $_[0]->scalingFactor);
