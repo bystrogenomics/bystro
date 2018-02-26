45902288d06012bd2e4bab855bbe018cb5c31d21
Alexei Kotlar <akotlar@bu.edu>
2018-02-26 22:27:15 +0000

    don't log so much

--- update-packages.sh
+++ update-packages.sh
@@ -4,24 +4,25 @@ cdir=$(pwd)
 
 git pull origin
 
+# Don't use -v because aws instance can become overwhelmed with the logs
 printf "\n\nInstalling bystro-utils/parse library\n\n"
 cd ~/go/src/github.com/akotlar/bystro-utils/parse
 git pull origin
-go install -v -race -a
+go install -a
 
 printf "\n\nInstalling bystro-stats library\n\n"
 cd ~/go/src/github.com/akotlar/bystro-stats
 git pull origin
-go install -v -race -a
+go install -a
 
 printf "\n\nInstalling bystro-vcf library\n\n"
 cd ~/go/src/github.com/akotlar/bystro-vcf/
 git pull origin
-go install -v -race -a
+go install -a
 
 printf "\n\nInstalling bystro-snp library\n\n"
 cd ../bystro-snp
 git pull origin
-go install -v -race -a
+go install -a
 
 cd $cdir
