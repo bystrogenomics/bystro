9a85bd9eed16c6dbb82038dde6e05dde22093061
Alexei Kotlar <akotlar@bu.edu>
2018-03-08 16:50:56 +0000

    overlap delimiter now used for overlapping records

--- lib/Seq/Output.pm
+++ lib/Seq/Output.pm
@@ -41,7 +41,7 @@ sub makeOutputString {
   my ($self, $outputDataAref) = @_;
 
   my $emptyFieldChar = $self->delimiters->emptyFieldChar;
-  my $alleleDelimiter = $self->delimiters->alleleDelimiter;
+  my $overlapDelimiter = $self->delimiters->overlapDelimiter;
   my $positionDelimiter = $self->delimiters->positionDelimiter;
   my $valueDelimiter = $self->delimiters->valueDelimiter;
   my $fieldSeparator = $self->delimiters->fieldSeparator;
@@ -73,7 +73,7 @@ sub makeOutputString {
                   defined $_
                   # Unfortunately, prior to 11/30/16 Bystro dbs would merge sparse tracks
                   # incorrectly, resulting in an extra array depth
-                  ? (ref $_ ? join($alleleDelimiter, map { defined $_ ? $_ : $emptyFieldChar } @$_) : $_)
+                  ? (ref $_ ? join($overlapDelimiter, map { defined $_ ? $_ : $emptyFieldChar } @$_) : $_)
                   : $emptyFieldChar
                 } @$positionData);
               }
