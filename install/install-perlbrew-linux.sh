2dd016d031106f7ace8e6840a09de2f46980f655
Alex Kotlar <akotlar@bu.edu>
2018-07-15 10:19:30 -0400

    fixes to perlbrew install

--- install/install-perlbrew-linux.sh
+++ install/install-perlbrew-linux.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 if [[ -n "$1" ]]
 then
   DIR=$1
@@ -9,12 +8,16 @@ fi
 
 echo -e "\n\nInstalling local perl via perlbrew into $DIR\n"
 
+export PERLBREW_ROOT=$DIR/perl5
+
 (\curl -L https://install.perlbrew.pl || \wget -O - https://install.perlbrew.pl) | bash
-(echo "" ; echo "source $DIR/perl5/perlbrew/etc/bashrc") | sudo tee -a $HOME/.bash_profile && source $HOME/.bash_profile;
+(echo "" ; echo "source $PERLBREW_ROOT/perlbrew/etc/bashrc") | sudo tee -a $HOME/.bash_profile
+
+source $HOME/.bash_profile;
 
 cnt=$(perlbrew list | grep perl-5.28.0 | wc -l)
 
-if [ $cnt > "0" ]; then
+if [ $cnt == 0 ]; then
   perlbrew install perl-5.28.0
 fi
 
@@ -23,5 +26,3 @@ perlbrew switch perl-5.28.0
 # Configure cpan to install locally
 # Not quite sure why not local by default
 ./install/configure-cpan.sh
-
-source $HOME/.bash_profile;
