fa55907080147004e7cab2e6cd00c3e2801c9669
Alex Kotlar <akotlar@bu.edu>
2018-10-08 23:58:00 -0400

    backport go dep

--- install/install-go-linux.sh
+++ install/install-go-linux.sh
@@ -17,12 +17,12 @@ echo -e "\n\nInstalling Go in /usr/local\n"
 
 # Clean in case somethign left over from old installation
 rm -rf go
-rm go1.10.3.linux-amd64.tar.gz;
-wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz;
-tar -xf go1.10.3.linux-amd64.tar.gz;
+rm go1.11.1.linux-amd64.tar.gz;
+wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz;
+tar -xf go1.11.1.linux-amd64.tar.gz;
 echo "Deleting go in /usr/local"
 sudo rm -rf /usr/local/go
 sudo mv go /usr/local;
-rm go1.10.3.linux-amd64.tar.gz;
+rm go1.11.1.linux-amd64.tar.gz;
 
 . install/export-go-path-linux.sh $DIR $PROFILE
