45742bce7ad30752e2e570deaf544fc11f917089
Alex Kotlar <akotlar@bu.edu>
2019-07-13 18:11:11 +0000

    remove chmod

--- lib/Seq/Definition.pm
+++ lib/Seq/Definition.pm
@@ -185,7 +185,7 @@ sub _moveFilesToOutputDir {
 
   my $workDir = $self->_workingDir->stringify();
 
-  $err = $self->safeSystem("mv $workDir/* $outDir && chmod -R a=rX $outDir; sync");
+  $err = $self->safeSystem("mv $workDir/* $outDir; sync");
 
   return $err;
 }
