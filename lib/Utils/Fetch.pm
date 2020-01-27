41eabe9637125ef134c69a6db7165e409da24cd8
Alex Kotlar <akotlar@bu.edu>
2020-01-27 02:51:41 +0000

    improve hg19.clean.yml, fetch.pm

--- lib/Utils/Fetch.pm
+++ lib/Utils/Fetch.pm
@@ -26,6 +26,7 @@ use DDP;
 # has fetch_program => (is => 'ro', writer => '_setFetchProgram');
 # has fetch_program_arguments => (is => 'ro', writer => '_setFetchProgramArguments');
 # has fetch_command => (is => 'ro');
+has aws => (is => 'ro', init_arg => undef, writer => '_setAws');
 has wget => (is => 'ro', init_arg => undef, writer => '_setWget');
 has rsync => (is => 'ro', init_arg => undef, writer => '_setRsync');
 
@@ -40,6 +41,7 @@ sub BUILD {
     $self->log('fatal', 'Fetch.pm requires rsync and wget when fetching remote_files');
   }
 
+  $self->_setAws(which('aws'));
   $self->_setRsync(which('rsync'));
   $self->_setWget(which('wget'));
 }
@@ -124,17 +126,20 @@ sub _fetchFiles {
 
   my $fetchProgram;
 
-  my $isRsync;
+  my $isRsync = 0;
+  my $isS3 = 0;
 
   if($self->_wantedTrack->{remote_dir}) {
     # remove http:// (or whatever protocol)
     $self->_wantedTrack->{remote_dir} =~ m/$pathRe/;
 
     if($1) {
-      $isRsync = 0;
       $remoteProtocol = $1;
+    } elsif($self->_wantedTrack->{remote_dir} =~ 's3://') {
+      $isS3 = 1;
+      $remoteProtocol = 's3://';
     } else {
-      $isRsync = 0;
+      $isRsync = 1;
       $remoteProtocol = 'rsync://';
     }
 
@@ -156,7 +161,9 @@ sub _fetchFiles {
       # This file is an absolute remote path
       if($1) {
         $remoteUrl = $file;
-        $isRsync = 0;
+      } elsif($file =~ 's3://') {
+        $isS3 = 1;
+        $remoteUrl = $file;
       } else {
         $remoteUrl = "rsync://" . $2;
         $isRsync = 1;
@@ -168,6 +175,12 @@ sub _fetchFiles {
 
     if($isRsync) {
       $command = $self->rsync . " -avPz $remoteUrl $outDir";
+    } elsif($isS3) {
+      if(!$self->aws) {
+        $self->log('fatal', "You requested an s3 remote file ($remoteUrl), but have no aws s3 cli installed. Please visit: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html");
+        return;
+      }
+      $command = $self->aws . " s3 cp $remoteUrl $outDir";
     } else {
       $command = $self->wget . " -N -S $remoteUrl -P $outDir";
     }
