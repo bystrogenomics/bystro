9598a86448ae267610a8b228d3c32235f66b198f
Alex Kotlar <akotlar@bu.edu>
2023-03-19 03:39:41 +0000

    fix test_bystro_indexer.sh

--- search2/python/search/index/t/test_bystro_indexer.sh
+++ search2/python/search/index/t/test_bystro_indexer.sh
@@ -3,4 +3,4 @@
 ## TODO: add beanstalkd local server and trigger submit of job
 . ./test_opensearch_server.sh
 . ../../../../.initialize_conda_env.sh
-python ../listener.py --conf_dir ./hg19.yml --queue_conf  --search_conf ./test_hg19.mapping.yml --queue_conf ./beanstalk.yml
+python ../listener.py --conf_dir ./ --queue_conf  --search_conf ./test_hg19.mapping.yml --queue_conf ./beanstalk.yml
