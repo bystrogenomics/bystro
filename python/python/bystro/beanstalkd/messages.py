8fa56166794e7bf5bc139154904ff7bb32af71e3
Alex Kotlar <akotlar@bu.edu>
2023-06-07 02:22:42 +0000

    track beanstalkd in git

---
+++ python/python/bystro/beanstalkd/messages.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from typing import get_type_hints
+
+from msgspec import Struct, field
+
+SubmissionID = str | int
+BeanstalkJobID = int
+
+
+class BaseMessage(Struct, frozen=True):
+    submissionID: SubmissionID
+
+    @classmethod
+    def keys_with_types(cls) -> dict:
+        return get_type_hints(cls)
+
+
+class FailedJobMessage(Struct, frozen=True):
+    submissionID: SubmissionID
+    reason: str
+
+
+class InvalidJobMessage(Struct, frozen=True):
+    # Invalid jobs that are invalid because the submission breaks serialization invariants
+    # will not have a submissionID as that ID is held in the serialized data
+    queueID: BeanstalkJobID
+    reason: str
+
+
+class Event(str, Enum):
+    """Beanstalkd Event"""
+
+    PROGRESS = "progress"
+    FAILED = "failed"
+    STARTED = "started"
+    COMPLETED = "completed"
+
+
+class ProgressData(Struct):
+    progress: int = 0
+    skipped: int = 0
+
+
+class ProgressMessage(BaseMessage, frozen=True):
+    """Beanstalkd Message"""
+
+    event: str = Event.PROGRESS
+    data: ProgressData = field(default_factory=ProgressData)
