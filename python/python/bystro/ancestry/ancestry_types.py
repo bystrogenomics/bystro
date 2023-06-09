ad9957cd57c01261a4bd561251b3171fac8771b4
Pat <pkoneil@emory.edu>
2023-06-09 01:59:20 -0400

    Add ancestry record classes (#122)
    Add classes holding common shapes of data for ancestry worker, tests for
    them.

---
+++ python/python/bystro/ancestry/ancestry_types.py
@@ -0,0 +1,201 @@
+"""Classes for common shapes of data in ancestry."""
+import re
+from collections import Counter
+from collections.abc import Sequence
+from typing import TypeVar
+
+import attrs
+from attrs import field
+from attrs.validators import ge, instance_of, le
+
+# Attr classes can throw either TypeError or ValueError upon receipt
+# of bad data.  Generally we won't care which, so define the union of
+# the two exceptions to catch all validation errors.  Because we'll be
+# using it in try blocks, this wants to be a tuple rather than a union.
+
+AttrValidationError = (ValueError, TypeError)
+
+
+#  It seems a bit overkill to have an attr class just for vcfs, but
+#  the validator needs to be shared between classes. So it just lives
+#  here as a pure python function with a weird type signature in order
+#  to support what attr expects from its validators.
+
+_VCF_REGEX = re.compile(
+    r"""
+    .*                        # anything
+    \.vcf                     # .vcf extension
+    (\.(gz|lz4|zstd|bzip2))?  # optional compression extension
+    $                         # and nothing else afterwards
+    """,
+    re.VERBOSE,
+)
+
+
+def _vcf_validator(_self: object, _attribute: attrs.Attribute, vcf_path: str) -> None:
+    if not isinstance(vcf_path, str):
+        err_msg = f"vcf_path must be of type str, got: {vcf_path} instead"
+        raise TypeError(err_msg)
+    if not _VCF_REGEX.fullmatch(vcf_path):
+        err_msg = (
+            "vcf_path must be of extension .vcf, .vcf.gz, .vcf.lz4 .vcf.zstd or .vcf.bzip2, "
+            f"got {vcf_path} instead"
+        )
+        raise ValueError(err_msg)
+
+
+@attrs.frozen()
+class AncestrySubmission:
+    """Represent an incoming submission to the ancestry worker."""
+
+    vcf_path: str = field(validator=_vcf_validator)
+
+
+unit_float_validator = [
+    instance_of(float),
+    ge(0.0),
+    le(1.0),
+]
+
+
+@attrs.frozen()
+class ProbabilityInterval:
+    """Represent an interval of probabilities."""
+
+    lower_bound: float = field(validator=unit_float_validator)
+    upper_bound: float = field(
+        validator=[
+            instance_of(float),
+            attrs.validators.ge(0.0),
+            attrs.validators.le(1.0),
+        ]
+    )
+
+    def __attrs_post_init__(self) -> None:
+        """Ensure interval is well-formed."""
+        if self.lower_bound > self.upper_bound:
+            err_msg = f"""Lower bound must be less than or equal to upper bound.
+            Got: lower_bound={self.lower_bound}, upper_bound={self.upper_bound} instead."""
+            raise ValueError(err_msg)
+
+
+# NB: We might consider that a vector of ProbabilityIntervals should
+# have additional validation properties, like that the sums of the
+# lower bounds, upper bounds, or midpoints should be close to one.
+# But constraints on the bounds don't hold in general (consider the
+# vector of intervals [(0.4, 0.6), (0.4, 0.6)]), and we can't know how
+# well the midpoints of the intervals reflect the point estimate in
+# general, so we'll punt on this and assume it's the ML model's
+# responsibility to give us scientifically sensible results.
+
+
+# this definition is mildly ugly but the alternative is to
+# generate it dynamically, which would be even worse...
+
+ProbIntValidator = attrs.validators.instance_of(ProbabilityInterval)
+
+
+@attrs.frozen(kw_only=True)
+class PopulationVector:
+    """A vector of probability intervals over populations.
+
+    Represents model estimates of an individual's similarity to
+    reference HapMap populations, with upper and lower bounds for each
+    population.
+    """
+
+    ACB: ProbabilityInterval = field(validator=ProbIntValidator)
+    ASW: ProbabilityInterval = field(validator=ProbIntValidator)
+    BEB: ProbabilityInterval = field(validator=ProbIntValidator)
+    CDX: ProbabilityInterval = field(validator=ProbIntValidator)
+    CEU: ProbabilityInterval = field(validator=ProbIntValidator)
+    CHB: ProbabilityInterval = field(validator=ProbIntValidator)
+    CHS: ProbabilityInterval = field(validator=ProbIntValidator)
+    CLM: ProbabilityInterval = field(validator=ProbIntValidator)
+    ESN: ProbabilityInterval = field(validator=ProbIntValidator)
+    FIN: ProbabilityInterval = field(validator=ProbIntValidator)
+    GBR: ProbabilityInterval = field(validator=ProbIntValidator)
+    GIH: ProbabilityInterval = field(validator=ProbIntValidator)
+    GWD: ProbabilityInterval = field(validator=ProbIntValidator)
+    IBS: ProbabilityInterval = field(validator=ProbIntValidator)
+    ITU: ProbabilityInterval = field(validator=ProbIntValidator)
+    JPT: ProbabilityInterval = field(validator=ProbIntValidator)
+    KHV: ProbabilityInterval = field(validator=ProbIntValidator)
+    LWK: ProbabilityInterval = field(validator=ProbIntValidator)
+    MAG: ProbabilityInterval = field(validator=ProbIntValidator)
+    MSL: ProbabilityInterval = field(validator=ProbIntValidator)
+    MXL: ProbabilityInterval = field(validator=ProbIntValidator)
+    PEL: ProbabilityInterval = field(validator=ProbIntValidator)
+    PJL: ProbabilityInterval = field(validator=ProbIntValidator)
+    PUR: ProbabilityInterval = field(validator=ProbIntValidator)
+    STU: ProbabilityInterval = field(validator=ProbIntValidator)
+    TSI: ProbabilityInterval = field(validator=ProbIntValidator)
+    YRI: ProbabilityInterval = field(validator=ProbIntValidator)
+
+
+@attrs.frozen(kw_only=True)
+class SuperpopVector:
+    """A vector of probability intervals for superpopulations.
+
+    Represents model estimates of an individual's similarity to
+    reference HapMap superpopulations, with upper and lower bounds for
+    each population.
+
+    """
+
+    AFR: ProbabilityInterval = field(validator=ProbIntValidator)
+    AMR: ProbabilityInterval = field(validator=ProbIntValidator)
+    EAS: ProbabilityInterval = field(validator=ProbIntValidator)
+    EUR: ProbabilityInterval = field(validator=ProbIntValidator)
+    SAS: ProbabilityInterval = field(validator=ProbIntValidator)
+
+
+@attrs.frozen(kw_only=True)
+class AncestryResult:
+    """An ancestry result from a sample.
+
+    Represents ancestry model output for an individual study
+    participant (identified by sample_id) with estimates for
+    populations and superpopulations, and the overall fraction of
+    expected variants found missing in the sample.
+    """
+
+    sample_id: str = field(validator=instance_of(str))
+    populations: PopulationVector = field(validator=instance_of(PopulationVector))
+    superpops: SuperpopVector = field(validator=instance_of(SuperpopVector))
+    missingness: float = field(validator=unit_float_validator)
+
+
+@attrs.frozen(kw_only=True)
+class AncestryResponse:
+    """An outgoing response from the ancestry worker.
+
+    Represents ancestry model output for an entire study as a list of
+    individual AncestryResults.
+
+    """
+
+    vcf_path: str = field(validator=_vcf_validator)
+    results: list[AncestryResult] = field(validator=instance_of(list))
+
+    @results.validator
+    def _is_list_of_unique_ancestry_results(
+        self, _attribute: attrs.Attribute, results: list[AncestryResult]
+    ) -> None:
+        for i, value in enumerate(results):
+            if not isinstance(value, AncestryResult):
+                err_msg = f"Expecting list of AncestryResults, at position {i} got {value} instead"
+                raise TypeError(err_msg)
+        sample_ids = [result.sample_id for result in results]
+        unique_sample_ids = set(sample_ids)
+        if len(unique_sample_ids) < len(results):
+            duplicates = _get_duplicates(sample_ids)
+            err_msg = f"Expected unique sample_ids but found duplicated samples {duplicates}"
+            raise ValueError(err_msg)
+
+
+T = TypeVar("T")
+
+
+def _get_duplicates(xs: Sequence[T]) -> set[T]:
+    return {x for x, count in Counter(xs).items() if count > 1}
---
+++ python/python/bystro/ancestry/tests/test_ancestry_types.py
@@ -0,0 +1,279 @@
+"""Test ancestry_types.py."""
+import pytest
+from attrs.exceptions import FrozenInstanceError
+
+from ..ancestry_types import (
+    AncestryResponse,
+    AncestryResult,
+    AncestrySubmission,
+    AttrValidationError,
+    PopulationVector,
+    ProbabilityInterval,
+    SuperpopVector,
+)
+
+POPULATIONS = [
+    "ACB",
+    "ASW",
+    "BEB",
+    "CDX",
+    "CEU",
+    "CHB",
+    "CHS",
+    "CLM",
+    "ESN",
+    "FIN",
+    "GBR",
+    "GIH",
+    "GWD",
+    "IBS",
+    "ITU",
+    "JPT",
+    "KHV",
+    "LWK",
+    "MAG",
+    "MSL",
+    "MXL",
+    "PEL",
+    "PJL",
+    "PUR",
+    "STU",
+    "TSI",
+    "YRI",
+]
+
+SUPERPOPS = [
+    "AFR",
+    "AMR",
+    "EAS",
+    "EUR",
+    "SAS",
+]
+
+
+def test_AncestrySubmission_accepts_valid_vcf_paths():
+    AncestrySubmission("foo.vcf")
+    AncestrySubmission("foo.vcf.gz")
+    AncestrySubmission("foo.vcf.lz4")
+    AncestrySubmission("foo.vcf.zstd")
+    AncestrySubmission("foo.vcf.bzip2")
+
+
+def test_AncestrySubmission_rejects_bad_vcf_paths():
+    with pytest.raises(AttrValidationError):
+        AncestrySubmission(3)
+    with pytest.raises(AttrValidationError):
+        AncestrySubmission("foo.txt")
+    with pytest.raises(AttrValidationError):
+        AncestrySubmission("foo.gz")
+    with pytest.raises(AttrValidationError):
+        AncestrySubmission("foo.vcf.docx")
+    with pytest.raises(AttrValidationError):
+        AncestrySubmission("foo.txt.gz")
+
+
+def test_AncestrySubmission_is_frozen():
+    ancestry_submission = AncestrySubmission("foo.vcf")
+    with pytest.raises(FrozenInstanceError):
+        ancestry_submission.vcf_path = "bar.vcf"
+
+
+prob_int = ProbabilityInterval(lower_bound=0.0, upper_bound=1.0)
+
+
+def test_ProbabilityInterval_accepts_valid_bounds() -> None:
+    """Ensure we can instantiate, validate ProbabilityInterval correctly."""
+    ProbabilityInterval(lower_bound=0.1, upper_bound=0.9)
+
+
+def test_ProbabilityInterval_rejects_bad_bounds() -> None:
+    with pytest.raises(AttrValidationError):
+        ProbabilityInterval(lower_bound=0.1, upper_bound=1.1)
+
+    with pytest.raises(AttrValidationError):
+        ProbabilityInterval(lower_bound=-2, upper_bound=1.1)
+
+    with pytest.raises(AttrValidationError):
+        ProbabilityInterval(lower_bound=1, upper_bound=0)
+
+    with pytest.raises(FrozenInstanceError):
+        prob_int.lower_bound = 0.5  # type: ignore [misc]
+
+
+pop_kwargs = {pop: prob_int for pop in POPULATIONS}
+superpop_kwargs = {pop: prob_int for pop in SUPERPOPS}
+
+
+def test_PopulationVector_accepts_valid_args() -> None:
+    """Ensure we can instantiate, validate PopulationVector correctly."""
+    PopulationVector(**pop_kwargs)
+
+
+def test_PopulationVector_rejects_missing_key() -> None:
+    pop_kwargs_with_missing_key = pop_kwargs.copy()
+    del pop_kwargs_with_missing_key["ACB"]
+    with pytest.raises(AttrValidationError):
+        PopulationVector(**pop_kwargs_with_missing_key)
+
+
+def test_PopulationVector_rejects_extra_key() -> None:
+    pop_kwargs_with_extra_key = pop_kwargs.copy()
+    pop_kwargs_with_extra_key["FOO"] = prob_int
+    with pytest.raises(AttrValidationError):
+        PopulationVector(**pop_kwargs_with_extra_key)
+
+
+def test_PopulationVector_is_frozen() -> None:
+    population_vector = PopulationVector(**pop_kwargs)
+    with pytest.raises(FrozenInstanceError):
+        population_vector.ACB = prob_int  # type: ignore [misc]
+
+
+def test_SuperpopVector_accepts_valid_args() -> None:
+    prob_int = ProbabilityInterval(lower_bound=0.0, upper_bound=1.0)
+    SuperpopVector(
+        AFR=prob_int,
+        AMR=prob_int,
+        EAS=prob_int,
+        EUR=prob_int,
+        SAS=prob_int,
+    )
+
+
+def test_SuperpopVector_rejects_missing_key() -> None:
+    with pytest.raises(AttrValidationError):
+        SuperpopVector(  # type: ignore [call-arg]
+            AFR=prob_int,
+            AMR=prob_int,
+            EAS=prob_int,
+            EUR=prob_int,
+        )
+
+
+def test_SuperpopVector_extra_key() -> None:
+    with pytest.raises(AttrValidationError):
+        SuperpopVector(  # type: ignore [call-arg]
+            AFR=prob_int,
+            AMR=prob_int,
+            EAS=prob_int,
+            EUR=prob_int,
+            SAS=prob_int,
+            FOO=prob_int,
+        )
+
+
+def test_SuperpopVector_is_frozen() -> None:
+    superpop_vector = SuperpopVector(
+        AFR=prob_int,
+        AMR=prob_int,
+        EAS=prob_int,
+        EUR=prob_int,
+        SAS=prob_int,
+    )
+
+    with pytest.raises(FrozenInstanceError):
+        superpop_vector.AFR = prob_int  # type: ignore [misc]
+
+
+def test_AncestryResult_accepts_valid_args() -> None:
+    AncestryResult(
+        sample_id="my_sample_id",
+        populations=PopulationVector(**pop_kwargs),
+        superpops=SuperpopVector(**superpop_kwargs),
+        missingness=0.5,
+    )
+
+
+def test_AncestryResult_rejects_invalid_missingness() -> None:
+    with pytest.raises(AttrValidationError):
+        AncestryResult(
+            sample_id="my_sample_id",
+            populations=PopulationVector(**pop_kwargs),
+            superpops=SuperpopVector(**superpop_kwargs),
+            missingness=1.1,
+        )
+
+
+def test_AncestryResult_is_frozen() -> None:
+    ancestry_result = AncestryResult(
+        sample_id="my_sample_id",
+        populations=PopulationVector(**pop_kwargs),
+        superpops=SuperpopVector(**superpop_kwargs),
+        missingness=0.1,
+    )
+    with pytest.raises(FrozenInstanceError):
+        ancestry_result.missingness = 0.2  # type: ignore [misc]
+
+
+def test_AncestryResponse_accepts_valid_args() -> None:
+    AncestryResponse(
+        vcf_path="myfile.vcf",
+        results=[
+            AncestryResult(
+                sample_id="foo",
+                populations=PopulationVector(**pop_kwargs),
+                superpops=SuperpopVector(**superpop_kwargs),
+                missingness=0.5,
+            ),
+            AncestryResult(
+                sample_id="bar",
+                populations=PopulationVector(**pop_kwargs),
+                superpops=SuperpopVector(**superpop_kwargs),
+                missingness=0.5,
+            ),
+            AncestryResult(
+                sample_id="baz",
+                populations=PopulationVector(**pop_kwargs),
+                superpops=SuperpopVector(**superpop_kwargs),
+                missingness=0.5,
+            ),
+        ],
+    )
+
+
+def test_AncestryResponse_rejects_bad_vcf_path() -> None:
+    with pytest.raises(AttrValidationError):
+        AncestryResponse(vcf_path="foo.txt", results=[])
+
+
+def test_AncestryResponse_rejects_bad_results_type() -> None:
+    with pytest.raises(AttrValidationError):
+        AncestryResponse(
+            vcf_path="myfile.vcf",
+            results=[3, 4, 5],  # type: ignore [list-item]
+        )
+
+
+def test_AncestryResponse_is_frozen() -> None:
+    ancestry_response = AncestryResponse(vcf_path="foo.vcf", results=[])
+    with pytest.raises(FrozenInstanceError):
+        ancestry_response.vcf_path = "bar.vcf"  # type: ignore [misc]
+
+
+def test_AncestryResponse_rejects_duplicate_sample_ids() -> None:
+    with pytest.raises(
+        AttrValidationError, match=r"Expected unique sample_ids but found duplicated samples {'foo'}"
+    ):
+        AncestryResponse(
+            vcf_path="myfile.vcf",
+            results=[
+                AncestryResult(
+                    sample_id="foo",
+                    populations=PopulationVector(**pop_kwargs),
+                    superpops=SuperpopVector(**superpop_kwargs),
+                    missingness=0.5,
+                ),
+                AncestryResult(
+                    sample_id="foo",
+                    populations=PopulationVector(**pop_kwargs),
+                    superpops=SuperpopVector(**superpop_kwargs),
+                    missingness=0.5,
+                ),
+                AncestryResult(
+                    sample_id="bar",
+                    populations=PopulationVector(**pop_kwargs),
+                    superpops=SuperpopVector(**superpop_kwargs),
+                    missingness=0.5,
+                ),
+            ],
+        )
