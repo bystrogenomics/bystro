920221fc96b07750474a2838ddfe7ca3ad9ac96c
Pat <pkoneil@emory.edu>
2023-06-12 10:20:53 -0400

    add rough train code (#129)
    Asking for very light code review on this one because this is not the
    final form of the ancestry model-- I just want to get it on GH so
    @cristinaetrv can start iterating on it (and in case 🚌)
    
    closes https://github.com/bystrogenomics/bystro/issues/127

---
+++ python/python/bystro/ancestry/asserts.py
@@ -0,0 +1,39 @@
+"""Convenience methods for safe asserts."""
+
+from typing import Any
+
+import numpy as np
+
+
+def assert_true(
+    description: str,
+    condition: bool | np.bool_,
+    comment: str = "",
+) -> None:
+    """Check that condition holds, raising AssertionError if not."""
+    if not condition:
+        msg = f"Expected {description}."
+        if comment:
+            msg += "  " + comment
+        raise AssertionError(msg)
+
+
+def assert_equals(
+    expected_description: str,
+    expected_value: Any,  # noqa: ANN401  Any is actually appropriate here
+    actual_description: str,
+    actual_value: Any,  # noqa: ANN401
+    comment: str = "",
+) -> None:
+    """Check that expected_value equals actual_value, raising AssertionError if not."""
+    comparison = expected_value == actual_value
+    success = all(comparison) if hasattr(comparison, "__len__") else comparison
+    if success:
+        return
+    msg = (
+        f"Expected {expected_description} ({expected_value}) "
+        f"to equal {actual_description} ({actual_value})."
+    )
+    if comment:
+        msg += "  " + comment
+    raise AssertionError(msg)
---
+++ python/python/bystro/ancestry/tests/test_train.py
@@ -0,0 +1,10 @@
+"""Tests for ancestry model training code."""
+
+from bystro.ancestry import train
+
+## this is not a very useful test, really just testing that train
+## loaded successfully, didn't fail on import
+
+
+def test_module_is_loaded_correctly():
+    assert "Ancestry" in train.__doc__
