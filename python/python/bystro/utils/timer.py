94fb69b8c6370c9653a9498726f56a78e59a793f
Pat O'Neill <pkoneil@emory.edu>
2023-08-08 14:46:22 -0400

    add module level docstring

--- python/python/bystro/utils/timer.py
+++ python/python/bystro/utils/timer.py
@@ -1,12 +1,14 @@
+"""Provide a timer as a contextmanager."""
+
 import time
 
 
 class Timer:
     def __enter__(self):
-        """Start a new timer as a context manager"""
-        self.start_time = time.time()
+        """Start a new timer as a context manager."""
+        self.start_time = time.perf_counter()
         return self
 
     def __exit__(self, *exc_info):
-        """Stop the context manager timer"""
-        self.elapsed_time = time.time() - self.start_time
+        """Stop the timer."""
+        self.elapsed_time = time.perf_counter() - self.start_time
