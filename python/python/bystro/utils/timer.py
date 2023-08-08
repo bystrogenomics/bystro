427510aab5e25237560119bbca60ef215bd50c5a
Pat O'Neill <pkoneil@emory.edu>
2023-08-08 14:33:40 -0400

    move test to test dir

--- python/python/bystro/utils/timer.py
+++ python/python/bystro/utils/timer.py
@@ -10,11 +10,3 @@ class Timer:
     def __exit__(self, *exc_info):
         """Stop the context manager timer"""
         self.elapsed_time = time.time() - self.start_time
-
-
-def test_Timer():
-    sleep_time = 0.1
-    with Timer() as timer:
-        time.sleep(sleep_time)
-    relative_error = (timer.elapsed_time - sleep_time) / sleep_time
-    assert abs(relative_error) < 0.1
