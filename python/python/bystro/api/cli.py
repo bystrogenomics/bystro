a3893039591f8db00f1fff9e87e9f9f9972ce8d3
Alex Kotlar <akotlar@bu.edu>
2023-08-15 11:57:34 +0000

    Working CLI tool

---
+++ python/python/bystro/api/cli.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+from multiprocessing import Value
+import os
+
+import requests
+
+STATE_FILE = "bystro_authentication_token.json"
+TOKEN_KEY = "access_token"
+JOB_TYPE_ROUTE_MAP = {
+    "all": "/list/all",
+    "public": "/list/all/public",
+    "shared": "/list/shared",
+    "incomplete": "/list/incomplete",
+    "completed": "/list/completed",
+    "failed": "/list/failed",
+}
+
+
+def load_state(state_dir: str):
+    path = os.path.join(state_dir, STATE_FILE)
+
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            return json.load(f)
+
+    return {}
+
+
+def save_state(data: object, state_dir: str, print_result=True):
+    save_path = os.path.join(state_dir, STATE_FILE)
+
+    with open(save_path, "w") as f:
+        json.dump(data, f)
+
+    if print_result:
+        print(
+            f"\nSaved authentication credentials to {save_path}:\n{json.dumps(data, indent=4)}"
+        )
+
+
+def signup(args: argparse.Namespace, print_result=True):
+    if print_result:
+        print(f"\nSigning up for Bystro with email: {args.email}, name: {args.name}")
+
+    url = f"{args.host}:{args.port}/api/user"
+
+    data = {"email": args.email, "name": args.name, "password": args.password}
+
+    response = requests.put(url, data=data)
+
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Login failed with response status: {response.status_code}. Error: \n{response.text}\n"
+        )
+
+    res = response.json()
+
+    # # Here you would actually make an API call to login. For now, we'll simulate.
+    save_state(
+        {
+            TOKEN_KEY: res[TOKEN_KEY],
+            "url": f"{args.host}:{args.port}",
+            "email": args.email,
+        },
+        args.dir,
+        print_result,
+    )
+
+    if print_result:
+        print("\nSignup & authentication successful. You may now use the Bystro API!\n")
+
+    return res
+
+
+def login(args: argparse.Namespace, print_result=True):
+    if print_result:
+        print(f"\nLogging into {args.host}:{args.port} with email: {args.email}.")
+
+    url = f"{args.host}:{args.port}/api/user/auth/local"
+
+    body = {"email": args.email, "password": args.password}
+
+    response = requests.post(url, data=body)
+
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Login failed with response status: {response.status_code}. Error: \n{response.text}\n"
+        )
+
+    res = response.json()
+
+    save_state(
+        {
+            TOKEN_KEY: res[TOKEN_KEY],
+            "url": args.host + ":" + str(args.port),
+            "email": args.email,
+        },
+        args.dir,
+        print_result,
+    )
+
+    if print_result:
+        print("\nLogin successful. You may now use the Bystro API!\n")
+
+    return res
+
+
+def authenticate(args):
+    state = load_state(args.dir)
+    url = state.get("url")
+    token = state.get(TOKEN_KEY)
+    email = state.get("email")
+
+    if not (url and token and email):
+        raise ValueError("\n\nYou are not logged in. Please login first.\n")
+
+    header = {"Authorization": f"Bearer {token}"}
+    return url, header, email
+
+
+def get_jobs(args: argparse.Namespace, print_result=True):
+    url, auth_header, _ = authenticate(args)
+    url = url + "/api/jobs"
+    job_type = args.type
+    job_id = args.id
+
+    if not (job_id or job_type):
+        raise ValueError("Please specify either a job id or a job type")
+
+    if job_id and job_type:
+        raise ValueError("Please specify either a job id or a job type, not both")
+
+    if not job_id and job_type not in JOB_TYPE_ROUTE_MAP.keys():
+        raise ValueError(
+            f"Invalid job type: {job_type}. Valid types are: {','.join(JOB_TYPE_ROUTE_MAP.keys())}"
+        )
+
+    url = url + f"/{job_id}" if job_id else url + JOB_TYPE_ROUTE_MAP[job_type]
+
+    if print_result:
+        if job_id:
+            print(f"\nFetching job with id:\t{job_id}")
+        else:
+            print(f"\nFetching jobs of type:\t{job_type}")
+
+    response = requests.get(url, headers=auth_header)
+
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Fetching jobs failed with response status: {response.status_code}. Error: {response.text}"
+        )
+
+    jobs = response.json()
+
+    if job_id:
+        # As mongo doesn't support keys with '.' in them, we need to store the config as a string
+        jobs["config"] = json.loads(jobs["config"])
+
+    if print_result:
+        print(f"\nJob(s) fetched successfully: \n")
+        print(json.dumps(jobs, indent=4))
+        print("\n")
+
+    return jobs
+
+
+def create_job(args: argparse.Namespace, print_result=True):
+    url, auth_header, _ = authenticate(args)
+    url = url + "/api/jobs/upload/"
+
+    payload = {
+        "job": json.dumps(
+            {
+                "assembly": args.assembly,
+                "options": {"index": args.index},
+            }
+        )
+    }
+
+    files = []
+    for file in args.files:
+        files.append(
+            (
+                "file",
+                (os.path.basename(file), open(file, "rb"), "application/octet-stream"),
+            )
+        )
+
+    if print_result:
+        print(f"\nCreating jobs for files: {','.join(map(lambda x: x[1][0], files))}\n")
+
+    response = requests.post(url, headers=auth_header, data=payload, files=files)
+
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Job creation failed with response status: {response.status_code}. Error: \n{response.text}\n"
+        )
+
+    if print_result:
+        print(f"\nJob creation successful:\n")
+        print(json.dumps(response.json(), indent=4))
+        print("\n")
+
+    return response.json()
+
+
+def get_user(args, print_result=True):
+    if print_result:
+        print("\n\nFetching user profile\n")
+
+    res = authenticate(args)
+
+    url, auth_header, email = res
+
+    response = requests.request("GET", url + "/api/user/me", headers=auth_header)
+
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Fetching profile failed with response status: {response.status_code}. Error: \n{response.text}\n"
+        )
+
+    if print_result:
+        print(f"\nFetched Profile for email {email}\n")
+        print(json.dumps(response.json(), indent=4))
+        print("\n")
+
+    return response.json()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="bystro-api", description="Bystro CLI tool for making API calls."
+    )
+    subparsers = parser.add_subparsers(title="commands")
+
+    # Adding the user sub-command
+    login_parser = subparsers.add_parser("login", help="Login to the system")
+    login_parser.add_argument("--host", required=True, help="Host of the system")
+    login_parser.add_argument(
+        "--port", type=int, default=443, help="Port of the system"
+    )
+    login_parser.add_argument("--email", required=True, help="Email to login with")
+    login_parser.add_argument("--password", required=True, help="Password to login")
+    login_parser.add_argument(
+        "--dir", default="./", help="Where to save Bystro API login state"
+    )
+    login_parser.set_defaults(func=login)
+
+    signup_parser = subparsers.add_parser("signup", help="Sign up to Bystro")
+    signup_parser.add_argument(
+        "--email",
+        required=True,
+        help="Email. This will serve as your unique username for login",
+    )
+    signup_parser.add_argument("--password", required=True, help="Password")
+    signup_parser.add_argument(
+        "--name",
+        required=True,
+        help="The name you'd like to use on the Bystro platform",
+    )
+    signup_parser.add_argument("--host", required=True, help="Host of the system")
+    signup_parser.add_argument(
+        "--port", type=int, default=443, help="Port of the system"
+    )
+    signup_parser.add_argument(
+        "--dir", default="./", help="Where to save Bystro API login state"
+    )
+    signup_parser.set_defaults(func=signup)
+
+    user_parser = subparsers.add_parser("get-user", help="Handle user operations")
+    user_parser.add_argument("--profile", action="store_true", help="Get user profile")
+    user_parser.add_argument(
+        "--dir", default="./", help="Where Bystro API login state is saved"
+    )
+    user_parser.set_defaults(func=get_user)
+
+    # Adding the jobs sub-command
+    create_jobs_parser = subparsers.add_parser("create-job", help="Create a job")
+    create_jobs_parser.add_argument(
+        "--files",
+        nargs="+",
+        required=True,
+        type=str,
+        help="Paths to files: .vcf and .snp formats accepted.",
+    )
+    create_jobs_parser.add_argument(
+        "--assembly",
+        type=str,
+        required=True,
+        help="Genome assembly (e.g., hg19 or hg38 for human genomes)",
+    )
+    create_jobs_parser.add_argument(
+        "--index",
+        type=bool,
+        default=True,
+        help="Whether or not to index the annotation",
+    )
+    create_jobs_parser.add_argument(
+        "--dir", default="./", help="Where Bystro API login state is saved"
+    )
+    create_jobs_parser.set_defaults(func=create_job)
+
+    jobs_parser = subparsers.add_parser(
+        "get-jobs", help="Fetch one job or a list of jobs"
+    )
+    jobs_parser.add_argument("--id", type=str, help="Get a specific job by ID")
+    jobs_parser.add_argument(
+        "--type",
+        choices=list(JOB_TYPE_ROUTE_MAP.keys()),
+        help="Get a list of jobs of a specific type",
+    )
+    jobs_parser.add_argument(
+        "--dir", default="./", help="Where Bystro API login state is saved"
+    )
+    jobs_parser.set_defaults(func=get_jobs)
+
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        try:
+            args.func(args)
+        except Exception as e:
+            print(f"\nSomething went wrong:\t{e}\n")
+
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/python/bystro/api/tests/test_cli.py
@@ -0,0 +1,275 @@
+import pytest
+import os
+
+from bystro.api.cli import (
+    load_state,
+    save_state,
+    signup,
+    login,
+    JOB_TYPE_ROUTE_MAP,
+    get_jobs,
+    create_job,
+    get_user,
+)
+
+from types import SimpleNamespace
+import json
+
+EXAMPLE_JOB = {
+    "_id": "64db4e67fb86b79cbda4f386",
+    "assembly": "hg38",
+    "options": {"index": True},
+    "inputQueryConfig": {"fieldNames": []},
+    "exports": {"archivedSubmissions": []},
+    "search": {
+        "fieldNames": [],
+        "indexConfig": [],
+        "archivedSubmissions": [],
+        "queries": [],
+    },
+    "type": "annotation",
+    "visibility": "private",
+    "ancestry": {},
+    "archivedSubmissions": [],
+    "actionsTaken": [],
+    "expireDate": "2023-08-22T10:07:35.865Z",
+    "userID": "64d6264274e4fab6baa0d198",
+    "inputFileName": "trio.trim.vep.vcf.gz",
+    "name": "trio.trim.vep.vcf",
+    "outputBaseFileName": "trio_trim_vep_vcf",
+    "submission": {
+        "state": "started",
+        "attempts": 1,
+        "log": {
+            "progress": 0,
+            "skipped": 0,
+            "messages": [
+                "Job Submitted!",
+            ],
+        },
+        "addedFileNames": [],
+        "_id": "64db4e68fb86b79cbda4f387",
+        "type": "annotation",
+        "submittedDate": "2023-08-15T10:07:36.027Z",
+        "queueID": "1538",
+        "startedDate": "2023-08-15T10:07:37.045Z",
+    },
+    "config": json.dumps(
+        {
+            "database_dir": "hidden",
+            "files_dir": "hidden",
+            "chromosomes": [
+                "chr1",
+                "chr2",
+            ],
+            "assembly": "hg38",
+        }
+    ),
+}
+
+
+def test_load_state_existing_file(mocker):
+    mocker.patch("os.path.exists", return_value=True)
+    mocker.patch("builtins.open", mocker.mock_open(read_data='{"key": "value"}'))
+    result = load_state("./")
+    assert result == {"key": "value"}
+
+
+def test_load_state_no_file(mocker):
+    mocker.patch("os.path.exists", return_value=False)
+    result = load_state("./")
+    assert result == {}
+
+
+def test_save_state(mocker):
+    mock_open = mocker.patch("builtins.open", mocker.mock_open())
+    save_state({"key": "value"}, "./", print_result=False)
+    mock_open.assert_called_once_with("./bystro_authentication_token.json", "w")
+
+
+@pytest.mark.parametrize(
+    "status_code, exception_message",
+    [
+        (404, "Login failed with response status: 404. Error: \nerror\n"),
+        (500, "Login failed with response status: 500. Error: \nserver error\n"),
+    ],
+)
+def test_login_failure(mocker, status_code, exception_message):
+    mocker.patch(
+        "requests.post",
+        return_value=mocker.Mock(
+            status_code=status_code,
+            text="error" if status_code == 404 else "server error",
+        ),
+    )
+    args = SimpleNamespace(
+        host="localhost",
+        port=8080,
+        email="test@example.com",
+        password="password",
+        dir="./",
+    )
+    with pytest.raises(RuntimeError, match=exception_message):
+        login(args, print_result=False)
+
+
+def test_create_job(mocker):
+    mocker.patch(
+        "bystro.api.cli.authenticate",
+        return_value=(
+            "localhost:8080",
+            {"Authorization": "Bearer TOKEN"},
+            "test@example.com",
+        ),
+    )
+    mocker.patch(
+        "requests.post",
+        return_value=mocker.Mock(status_code=200, json=lambda: {"success": True}),
+    )
+    args = SimpleNamespace(
+        files=[os.path.join(os.path.dirname(__file__), "trio.trim.vep.short.vcf.gz")],
+        assembly="hg38",
+        index=True,
+        dir="./",
+    )
+    response = create_job(args, print_result=False)
+    assert response == {"success": True}
+
+
+def test_get_job_fail_validation(mocker):
+    mocker.patch(
+        "bystro.api.cli.authenticate",
+        return_value=(
+            "localhost:8080",
+            {"Authorization": "Bearer TOKEN"},
+            "test@example.com",
+        ),
+    )
+
+    with pytest.raises(
+        ValueError, match="Please specify either a job id or a job type"
+    ):
+        args = SimpleNamespace(dir="./", type=None, id=None)
+        get_jobs(args, print_result=False)
+
+    with pytest.raises(
+        ValueError, match="Please specify either a job id or a job type, not both"
+    ):
+        args = SimpleNamespace(dir="./", type="completed", id="1234")
+        get_jobs(args, print_result=False)
+
+    with pytest.raises(
+        ValueError,
+        match=f"Invalid job type: dasfa. Valid types are: {','.join(JOB_TYPE_ROUTE_MAP.keys())}",
+    ):
+        args = SimpleNamespace(dir="./", type="dasfa", id=None)
+        get_jobs(args, print_result=False)
+
+
+def test_get_job_list(mocker):
+    mocker.patch(
+        "bystro.api.cli.authenticate",
+        return_value=(
+            "localhost:8080",
+            {"Authorization": "Bearer TOKEN"},
+            "test@example.com",
+        ),
+    )
+    mocker.patch(
+        "requests.get", return_value=mocker.Mock(status_code=200, json=lambda: [])
+    )
+
+    args = SimpleNamespace(dir="./", type="completed", id=None)
+    print("args", args)
+    response = get_jobs(args, print_result=False)
+    assert response == []
+
+    args = SimpleNamespace(dir="./", type="failed", id=None)
+    response = get_jobs(args, print_result=False)
+    assert response == []
+
+    args = SimpleNamespace(dir="./", type="public", id=None)
+    response = get_jobs(args, print_result=False)
+    assert response == []
+
+    args = SimpleNamespace(dir="./", type="shared", id=None)
+    response = get_jobs(args, print_result=False)
+    assert response == []
+
+    args = SimpleNamespace(dir="./", type="all", id=None)
+    response = get_jobs(args, print_result=False)
+    assert response == []
+
+    args = SimpleNamespace(dir="./", type="incomplete", id=None)
+    response = get_jobs(args, print_result=False)
+    assert response == []
+
+
+def test_get_job(mocker):
+    mocker.patch(
+        "bystro.api.cli.authenticate",
+        return_value=(
+            "localhost:8080",
+            {"Authorization": "Bearer TOKEN"},
+            "test@example.com",
+        ),
+    )
+
+    mocker.patch(
+        "requests.get",
+        return_value=mocker.Mock(status_code=200, json=lambda: EXAMPLE_JOB.copy()),
+    )
+
+    args = SimpleNamespace(dir="./", id="12341", type=None)
+    response = get_jobs(args, print_result=False)
+
+    parsed_job = EXAMPLE_JOB.copy()
+    parsed_job["config"] = json.loads(parsed_job["config"])
+
+    assert response == parsed_job
+
+
+def test_signup(mocker):
+    expected_response = {"access_token": "TOKEN"}
+    mocker.patch(
+        "bystro.api.cli.save_state",
+        return_value=(
+            "access_token",
+            {"Authorization": "Bearer TOKEN"},
+            "test@example.com",
+        ),
+    )
+    mocker.patch(
+        "requests.put",
+        return_value=mocker.Mock(status_code=200, json=lambda: expected_response),
+    )
+    args = SimpleNamespace(
+        dir="./",
+        email="test@example.com",
+        password="password",
+        name="test",
+        host="localhost",
+        port=8080,
+    )
+    response = signup(args, print_result=False)
+    assert response == expected_response
+
+
+def test_get_user(mocker):
+    mocker.patch(
+        "bystro.api.cli.authenticate",
+        return_value=(
+            "localhost:8080",
+            {"Authorization": "Bearer TOKEN"},
+            "test@example.com",
+        ),
+    )
+    mocker.patch(
+        "requests.request",
+        return_value=mocker.Mock(
+            status_code=200, json=lambda: {"email": "test@example.com"}
+        ),
+    )
+    args = SimpleNamespace(dir="./", email="test@example.com", password="password")
+    user = get_user(args, print_result=False)
+    assert user == {"email": "test@example.com"}
