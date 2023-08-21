22b4494cf4b4e0968a934b1e386bc83778bd99d2
Pat O'Neill <pkoneil@emory.edu>
2023-08-21 14:09:36 -0400

    typo

--- python/python/bystro/proteomics/fragpipe_tandem_mass_tag.py
+++ python/python/bystro/proteomics/fragpipe_tandem_mass_tag.py
@@ -20,7 +20,7 @@ def _check_df_cols(df: pd.DataFrame, expected_cols: list[str]) -> None:
     actual_cols = df.columns
     if not all(x == y for x, y in zip(expected_cols, actual_cols, strict=False)):
         err_msg = (
-            f"expected dataframe: to begin with cols: {expected_cols}, got cols: {actual_cols} instead."
+            f"expected dataframe to begin with cols: {expected_cols}, got cols: {actual_cols} instead."
         )
         raise ValueError(err_msg)
 
