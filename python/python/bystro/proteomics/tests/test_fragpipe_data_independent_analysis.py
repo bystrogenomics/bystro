10d324e290284d9be21ea2340f7584dd849eefa1
Pat O'Neill <pkoneil@emory.edu>
2023-08-22 15:05:33 -0400

    update test

--- python/python/bystro/proteomics/tests/test_fragpipe_data_independent_analysis.py
+++ python/python/bystro/proteomics/tests/test_fragpipe_data_independent_analysis.py
@@ -13,22 +13,22 @@ raw_pg_matrix_df = pd.DataFrame(
         "Protein.Names": {0: np.nan, 1: np.nan, 2: np.nan},
         "Genes": {0: "NUDT4B", 1: "IGKV3-7", 2: "IGLV4-69"},
         "First.Protein.Description": {0: np.nan, 1: np.nan, 2: np.nan},
-        "/storage/yihsiao/data/ccrcc_4plexes/CPTAC_CCRCC_W_JHU_20190112_LUMOS_C3N-01522_T.mzML": {
+        "foo.mzML": {
             0: 806691.0,
             1: 38656400.0,
             2: 129411.0,
         },
-        "/storage/yihsiao/data/ccrcc_4plexes/CPTAC_CCRCC_W_JHU_20190112_LUMOS_C3N-01179_NAT.mzML": {
+        "bar.mzML": {
             0: 1056910.0,
             1: 74868600.0,
             2: np.nan,
         },
-        "/storage/yihsiao/data/ccrcc_4plexes/CPTAC_CCRCC_W_JHU_20190112_LUMOS_C3N-01522_NAT.mzML": {
+        "baz.mzML": {
             0: 1530830.0,
             1: 56854300.0,
             2: np.nan,
         },
-        "/storage/yihsiao/data/ccrcc_4plexes/CPTAC_CCRCC_W_JHU_20190112_LUMOS_C3N-01648_NAT.mzML": {
+        "quux.mzML": {
             0: 1337020.0,
             1: 65506700.0,
             2: 478757.0,
