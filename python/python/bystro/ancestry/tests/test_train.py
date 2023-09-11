b8bd80fa9f6781102c32dac2ff02a3f78c713a79
Pat O'Neill <pkoneil@emory.edu>
2023-09-11 14:25:19 -0400

    handle missing data in vcf

--- python/python/bystro/ancestry/tests/test_train.py
+++ python/python/bystro/ancestry/tests/test_train.py
@@ -15,12 +15,9 @@ from bystro.ancestry.train import (
     convert_1kgp_vcf_to_dosage,
     process_vcf_for_pc_transformation,
     restrict_loadings_variants_to_vcf,
-    apply_pca_transform
+    apply_pca_transform,
 )
-from bystro.vcf_utils.simulate_random_vcf import (
-    generate_simulated_vcf,
-    convert_sim_vcf_to_df
-)
+from bystro.vcf_utils.simulate_random_vcf import generate_simulated_vcf, convert_sim_vcf_to_df
 
 
 def test__parse_vcf_from_file_stream():
@@ -33,6 +30,31 @@ def test__parse_vcf_from_file_stream():
     ]
     expected_df = pd.DataFrame(
         [[1, 0, 1], [1, 2, 1], [0, 2, 0]],
+        index=["sample1", "sample2", "sample3"],
+        columns=["chr1:1:T:G", "chr1:123:T:G", "chr1:123456:T:G"],
+    )
+    actual_df = _parse_vcf_from_file_stream(
+        file_stream,
+        [
+            "chr1:1:T:G",
+            "chr1:123:T:G",
+            "chr1:123456:T:G",
+        ],
+        return_exact_variants=True,
+    )
+    assert_frame_equal(expected_df, actual_df)
+
+
+def test__parse_vcf_from_file_stream_missing_data():
+    file_stream = [
+        "##Some comment",
+        "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	sample1 sample2 sample3",
+        "chr1	1	.	T	G	.	PASS	i;n;f;o	GT	.|.	1|0	0|0",
+        "chr1	123	.	T	G	.	PASS	i;n;f;o	GT	0|0	.|1	1|1",
+        "chr1	123456	.	T	G	.	PASS	i;n;f;o	GT	0|1	1|0	0|.",
+    ]
+    expected_df = pd.DataFrame(
+        [[np.nan, 0, 1], [1, np.nan, 1], [0, 2, np.nan]],
         index=["sample1", "sample2", "sample3"],
         columns=["chr1:1:T:G", "chr1:123:T:G", "chr1:123456:T:G"],
     )
@@ -267,7 +289,7 @@ def test_superpop_predictions_from_pop_probs():
     ]
     assert expected_superpop_predictions == superpop_predictions
 
-    
+
 def test_1kgp_vcf_to_dosage():
     """Tests gnomad-filtered 1kgp vcf loading and processing"""
     # Pick num of samples for simulated VCF and loadings data
