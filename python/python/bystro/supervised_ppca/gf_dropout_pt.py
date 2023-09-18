0812d4556f7905930e46e29f89968ff3ead4dca3
Lobo <austin.talbot1993@gmail.com>
2023-09-18 16:05:21 -0400

    Addressed notation discrepancies

--- python/python/bystro/supervised_ppca/gf_dropout_pt.py
+++ python/python/bystro/supervised_ppca/gf_dropout_pt.py
@@ -4,6 +4,15 @@ to supervise a subset of latent variables to be predictive of an auxiliary
 outcome. Currently only implements logistic regression but in the future 
 will be modified to allow for more general predictive outcomes.
 
+Note that in the code W is a L x p array, where L is the latent dimensionality
+and  p is the covariate dimension, for implementation convenience. However,
+mathematically it is often pxL for notational convenience. Given that the
+most insidious errors are mathematical in nature rather than coding, (as faulty
+math is difficult to detect in unit tests), our notation matches mathematics
+rather than code, specifically when naming WWT and WTW to match the Bishop 2006
+notation rather than code.
+
+
 Objects
 -------
 PPCADropout(PPCA)
