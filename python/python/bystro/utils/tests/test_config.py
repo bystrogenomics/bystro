2a7b246f12969185f02ced44611d343cc243e1f6
Pat O'Neill <pkoneil@emory.edu>
2023-09-27 15:07:13 -0400

    mock Path.glob

--- python/python/bystro/utils/tests/test_config.py
+++ python/python/bystro/utils/tests/test_config.py
@@ -1,9 +1,20 @@
+import pathlib
+import pytest
+
 from bystro.utils.config import get_bystro_project_root, get_opensearch_config
+from unittest.mock import patch
 
 
 def test_get_bystro_project_root():
     bystro_project_root = get_bystro_project_root()
     assert "bystro" in str(bystro_project_root)
+
+
+@patch("pathlib.Path.glob", return_value=[])
+def test_get_bystro_project_root_error_case(mocked_glob):
+    """Test case where Path.glob never returns startup.yml."""
+    with pytest.raises(FileNotFoundError, match="this is a bug"):
+        bystro_project_root = get_bystro_project_root()
 
 
 def test_get_opensearch_config():
