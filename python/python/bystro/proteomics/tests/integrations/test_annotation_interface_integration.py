fda13a9bd328b92a6a758722d156269855eab7e4
Pat O'Neill <pkoneil@emory.edu>
2023-10-11 13:10:34 -0400

    add integration tests

--- python/python/bystro/proteomics/tests/integrations/test_annotation_interface_integration.py
+++ python/python/bystro/proteomics/tests/integrations/test_annotation_interface_integration.py
@@ -3,7 +3,7 @@ import pytest
 from opensearchpy import OpenSearch
 from bystro.utils.config import get_opensearch_config
 from bystro.search.utils.opensearch import gather_opensearch_args
-from bystro.proteomics.annotation_interface import get_samples_and_genes
+from bystro.proteomics.annotation_interface import get_samples_and_genes_from_query
 
 
 @pytest.mark.integration
@@ -15,7 +15,7 @@ def test_foo():
     # trim_trio_vep.vcf on bystro-dev.  If the index is no longer
     # present, this test may fail for no fault of the tested code's own.
     index_name = "64c889415acb6d3b3e40e07b_63ddc9ce1e740e0020c39928"
-    samples_genes_df = get_samples_and_genes(opensearch_client, user_query_string, index_name)
+    samples_genes_df = get_samples_and_genes_from_query(user_query_string, index_name, opensearch_client)
     assert samples_genes_df.shape == (1191, 3)
     assert {"1805", "1847", "4805"} == set(samples_genes_df.sample_id.unique())
     assert 689 == len(samples_genes_df.gene_name.unique())
