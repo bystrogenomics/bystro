b5580908ab059eaa67d93b6943a8debe0a4c0252
Pat O'Neill <pkoneil@emory.edu>
2023-10-24 16:22:44 -0400

    add uniprot_id / gene_name conversion materials

---
+++ python/python/bystro/proteomics/scripts/get_uniprot_id_gene_name_mapping.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""Download a list of uniprot_id / gene_name pairs from Uniprot and store the result on disk as csv."""
+
+from unipressed import UniprotkbClient
+
+from bystro.utils.config import BYSTRO_PROJECT_ROOT
+
+
+def get_uniprot_id_gene_name_mapping():
+    """Get a dataframe associating each uniprot id with its cognate gene names."""
+    APPROXIMATE_TOTAL_RECORDS = 240_000
+    query_results = UniprotkbClient.search(
+        query="(organism_name:homo)",
+    )
+    uniprot_id_gene_name_mapping = []
+    for record in tqdm.trange(query_results.each_record(), total=APPROXIMATE_TOTAL_RECORDS):
+        genes = record.get("genes", [])
+        gene_names = []
+        for gene in genes:
+            if gene_name := gene.get("geneName"):
+                if value := gene_name.get("value"):
+                    gene_names.append(value)
+        if not gene_names:
+            gene_names = [None]
+        for gene_name in gene_names:
+            uniprot_id_gene_name_mapping.append((record["primaryAccession"], gene_name))
+    uniprot_id_gene_name_mapping_df = pd.DataFrame(
+        uniprot_id_gene_name_mapping, columns=["uniprot_accession", "gene_name"]
+    )
+    return uniprot_id_gene_name_mapping_df
+
+
+if __name__ == "__main__":
+    uniprot_id_gene_name_mapping_df = get_uniprot_id_gene_name_mapping()
+    output_filename = (
+        BYSTRO_PROJECT_ROOT / "python/python/bystro/proteomics" / "uniprot_id_gene_name_mapping.csv"
+    )
+    uniprot_id_gene_name_mapping_df.to_csv(output_filename, index=False)
