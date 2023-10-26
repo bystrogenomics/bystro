489b585f1efe828038eafde3552cfddd5d2195ee
Alex Kotlar <akotlar@bu.edu>
2023-10-26 21:36:52 +0000

    format messages.py

--- python/python/bystro/search/utils/messages.py
+++ python/python/bystro/search/utils/messages.py
@@ -1,4 +1,9 @@
-from bystro.beanstalkd.messages import BaseMessage, SubmittedJobMessage, CompletedJobMessage, Struct
+from bystro.beanstalkd.messages import (
+    BaseMessage,
+    SubmittedJobMessage,
+    CompletedJobMessage,
+    Struct,
+)
 from bystro.search.utils.annotation import AnnotationOutputs
 
 
@@ -22,22 +27,31 @@ class IndexJobCompleteMessage(CompletedJobMessage, frozen=True, kw_only=True):
     results: IndexJobResults
 
 
-class BinomialMafFilter(Struct, frozen=True, tag='binomMaf', tag_field='key', forbid_unknown_fields=True):
+class BinomialMafFilter(
+    Struct, frozen=True, tag="binomMaf", tag_field="key", forbid_unknown_fields=True
+):
     """
-    Example: {'critValue': '.05', 'estimates': ['gnomad.genomes.af', 'gnomad.exomes.af'], 'privateMaf': 0.0001, 'snpOnly': True, 'numSamples': 3, 'key': 'binomialMaf'}
+    Example: {'critValue': '.05', 'estimates': ['gnomad.genomes.af', 'gnomad.exomes.af'],
+             'privateMaf': 0.0001, 'snpOnly': True, 'numSamples': 3, 'key': 'binomialMaf'}
     """
+
     privateMaf: float
     snpOnly: bool
     numSamples: int
     estimates: list[str]
-    critValue: float | None = .05
+    critValue: float | None = 0.05
 
-class HWEFilter(Struct, frozen=True, tag='hwe', tag_field='key', forbid_unknown_fields=True):
+
+class HWEFilter(
+    Struct, frozen=True, tag="hwe", tag_field="key", forbid_unknown_fields=True
+):
     """
     Example: {'critValue': '.05', 'numSamples': 3, 'key': 'hwe'}
     """
+
     numSamples: int
-    critValue: float | None = .05
+    critValue: float | None = 0.05
+
 
 class SaveJobData(BaseMessage, frozen=True):
     """Data for SaveFromQuery jobs received from beanstalkd"""
