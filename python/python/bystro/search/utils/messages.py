e7fd1955510a5215812fcba646be60b2b11ea3d9
Alex Kotlar <akotlar@bu.edu>
2023-10-26 18:07:05 +0000

    add pipeline tagged union type to SaveJobData

--- python/python/bystro/search/utils/messages.py
+++ python/python/bystro/search/utils/messages.py
@@ -22,6 +22,23 @@ class IndexJobCompleteMessage(CompletedJobMessage, frozen=True, kw_only=True):
     results: IndexJobResults
 
 
+class BinomialMafFilter(Struct, frozen=True, tag='binomMaf', tag_field='key', forbid_unknown_fields=True):
+    """
+    Example: {'critValue': '.05', 'estimates': ['gnomad.genomes.af', 'gnomad.exomes.af'], 'privateMaf': 0.0001, 'snpOnly': True, 'numSamples': 3, 'key': 'binomialMaf'}
+    """
+    privateMaf: float
+    snpOnly: bool
+    numSamples: int
+    estimates: list[str]
+    critValue: float | None = .05
+
+class HWEFilter(Struct, frozen=True, tag='hwe', tag_field='key', forbid_unknown_fields=True):
+    """
+    Example: {'critValue': '.05', 'numSamples': 3, 'key': 'hwe'}
+    """
+    numSamples: int
+    critValue: float | None = .05
+
 class SaveJobData(BaseMessage, frozen=True):
     """Data for SaveFromQuery jobs received from beanstalkd"""
 
@@ -30,6 +47,7 @@ class SaveJobData(BaseMessage, frozen=True):
     indexName: str
     outputBasePath: str
     fieldNames: list[str]
+    pipeline: list[BinomialMafFilter | HWEFilter] | None = None
 
 
 class SaveJobSubmitMessage(SubmittedJobMessage, frozen=True, kw_only=True):
