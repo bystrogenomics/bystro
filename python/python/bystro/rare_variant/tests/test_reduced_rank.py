4071c272fa43b341bea82788040b30fad569ab43
austintalbot <austin.talbot1993@gmail.com>
2023-11-17 20:39:48 -0500

    Added init and converted unit test to pytest

--- python/python/bystro/rare_variant/tests/test_reduced_rank.py
+++ python/python/bystro/rare_variant/tests/test_reduced_rank.py
@@ -1,40 +1,35 @@
-import unittest
 import numpy as np
-from bystro.rare_variant.reduced_rank import ReducedRankML  
+from bystro.rare_variant.reduced_rank import ReducedRankML
 
-class TestReducedRankML(unittest.TestCase):
+# Initialize data and model parameters for testing
 
-    def setUp(self):
-        # Initialize data and model parameters for testing
-        np.random.seed(42)
-        self.X = np.random.rand(100, 10)
-        self.Y = np.random.randint(2, size=(100, 5))
-        self.model = ReducedRankML(lamb_sparsity=1.0, lamb_rank=1.0)
+rng = np.random.default_rng(2021)
+X = rng.normal(size=(100, 10))
+Y = rng.binomial(1, 0.5, size=(100, 5))
 
-    def test_fit(self):
-        # Test the fit method
-        self.model.fit(self.X, self.Y)
-        
-        # Check if the model has been trained
-        self.assertTrue(hasattr(self.model, 'B_'))
-        self.assertTrue(hasattr(self.model, 'alpha_'))
 
-    def test_predict(self):
-        # Test the predict method
-        self.model.fit(self.X, self.Y)
-        predictions = self.model.predict(self.X)
+def test_fit():
+    model = ReducedRankML(lamb_sparsity=1.0, lamb_rank=1.0)
+    model.fit(X, Y)
 
-        # Check if the predictions have the correct shape
-        self.assertEqual(predictions.shape, (self.X.shape[0], self.Y.shape[1]))
+    # Check if the model has been trained
+    assert hasattr(model, "B_")
+    assert hasattr(model, "alpha_")
 
-    def test_decision_function(self):
-        # Test the decision_function method
-        self.model.fit(self.X, self.Y)
-        scores = self.model.decision_function(self.X)
 
-        # Check if the scores have the correct shape
-        self.assertEqual(scores.shape, (self.X.shape[0], self.Y.shape[1]))
+def test_predict():
+    model = ReducedRankML(lamb_sparsity=1.0, lamb_rank=1.0)
+    model.fit(X, Y)
+    predictions = model.predict(X)
 
-if __name__ == '__main__':
-    unittest.main()
+    # Check if the predictions have the correct shape
+    assert predictions.shape == (X.shape[0], Y.shape[1])
 
+
+def test_decision_function():
+    model = ReducedRankML(lamb_sparsity=1.0, lamb_rank=1.0)
+    model.fit(X, Y)
+    scores = model.decision_function(X)
+
+    # Check if the scores have the correct shape
+    assert scores.shape == (X.shape[0], Y.shape[1])
