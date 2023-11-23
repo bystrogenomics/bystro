5574f075ed078fc2fcc3c37ad05f429a44cedd5f
Alex Kotlar <akotlar@bu.edu>
2023-11-23 18:21:24 +0000

    run perl tidy

--- perl/bin/bystro-index-server.pl
+++ perl/bin/bystro-index-server.pl
@@ -31,34 +31,36 @@ use SeqElastic;
 
 # usage
 # Debug like verbose, but isn't passed through to SeqElastic
-my ($verbose, $queueConfigPath, $connectionConfigPath, $maxThreads, $debug);
+my ( $verbose, $queueConfigPath, $connectionConfigPath, $maxThreads, $debug );
 
 GetOptions(
-  'v|verbose=i'   => \$verbose,
-  'd|debug'      => \$debug,
-  'q|queueConfig=s'   => \$queueConfigPath,
-  'c|connectionConfig=s' => \$connectionConfigPath,
-  'm|maxThreads=i' => \$maxThreads,
+    'v|verbose=i'          => \$verbose,
+    'd|debug'              => \$debug,
+    'q|queueConfig=s'      => \$queueConfigPath,
+    'c|connectionConfig=s' => \$connectionConfigPath,
+    'm|maxThreads=i'       => \$maxThreads,
 );
 
-if ( !($queueConfigPath && $connectionConfigPath) ) {
-  # Generate a help strings taht shows the arguments
-  say "\nUsage: perl $0 -q <queueConfigPath> -c <connectionConfigPath> --maxThreads <maxThreads> --verbose <level> --debug\n";
-  exit 1;
+if ( !( $queueConfigPath && $connectionConfigPath ) ) {
+
+    # Generate a help strings taht shows the arguments
+    say
+"\nUsage: perl $0 -q <queueConfigPath> -c <connectionConfigPath> --maxThreads <maxThreads> --verbose <level> --debug\n";
+    exit 1;
 }
 
-my $conf = LoadFile($queueConfigPath);
+my $conf             = LoadFile($queueConfigPath);
 my $connectionConfig = LoadFile($connectionConfigPath);
 
 # Beanstalk servers will be sharded
-my $beanstalkHost  = $conf->{beanstalk_host_1};
-my $beanstalkPort  = $conf->{beanstalk_port_1};
+my $beanstalkHost = $conf->{beanstalk_host_1};
+my $beanstalkPort = $conf->{beanstalk_port_1};
 
 # Required fields
 # The annotation_file_path is constructed from inputDir, inputFileNames by SeqElastic
 my @requiredJobFields = qw/indexName inputDir inputFileNames assembly/;
 
-my $configPathBaseDir = "config/";
+my $configPathBaseDir  = "config/";
 my $configFilePathHref = {};
 use DDP;
 
@@ -69,193 +71,204 @@ my $FAILED    = "failed";
 my $STARTED   = "started";
 my $COMPLETED = "completed";
 
-my $beanstalk = Beanstalk::Client->new({
-  server    => $conf->{beanstalkd}{addresses}[0],
-  default_tube => $queueConfig->{submission},
-  connect_timeout => 1,
-  encoder => sub { encode_json(\@_) },
-  decoder => sub { @{decode_json(shift)} },
-});
+my $beanstalk = Beanstalk::Client->new(
+    {
+        server          => $conf->{beanstalkd}{addresses}[0],
+        default_tube    => $queueConfig->{submission},
+        connect_timeout => 1,
+        encoder         => sub { encode_json( \@_ ) },
+        decoder         => sub { @{ decode_json(shift) } },
+    }
+);
 
-my $beanstalkEvents = Beanstalk::Client->new({
-  server    => $conf->{beanstalkd}{addresses}[0],
-  default_tube => $queueConfig->{events},
-  connect_timeout => 1,
-  encoder => sub { encode_json(\@_) },
-  decoder => sub { @{decode_json(shift)} },
-});
+my $beanstalkEvents = Beanstalk::Client->new(
+    {
+        server          => $conf->{beanstalkd}{addresses}[0],
+        default_tube    => $queueConfig->{events},
+        connect_timeout => 1,
+        encoder         => sub { encode_json( \@_ ) },
+        decoder         => sub { @{ decode_json(shift) } },
+    }
+);
 
 my $events = $conf->{beanstalkd}{events};
 
-while(my $job = $beanstalk->reserve) {
-  # Parallel ForkManager used only to throttle number of jobs run in parallel
-  # cannot use run_on_finish with blocking reserves, use try catch instead
-  # Also using forks helps clean up leaked memory from LMDB_File
-  # Unfortunately, parallel fork manager doesn't play nicely with try tiny
-  # prevents anything within the try from executing
+while ( my $job = $beanstalk->reserve ) {
 
-  my $jobDataHref;
-  my ($err, $fieldNames, $searchConfigHashRef);
+    # Parallel ForkManager used only to throttle number of jobs run in parallel
+    # cannot use run_on_finish with blocking reserves, use try catch instead
+    # Also using forks helps clean up leaked memory from LMDB_File
+    # Unfortunately, parallel fork manager doesn't play nicely with try tiny
+    # prevents anything within the try from executing
 
-  try {
-    $jobDataHref = decode_json( $job->data );
+    my $jobDataHref;
+    my ( $err, $fieldNames, $searchConfigHashRef );
 
-    $beanstalkEvents->put(
-      {
-        priority => 0,
-        data     => encode_json(
-          {
-            event        => $STARTED,
-            submissionID => $jobDataHref->{submissionID},
-            queueID      => $job->id,
-          }
-        )
-      }
-    );
+    try {
+        $jobDataHref = decode_json( $job->data );
 
-    ($err, $fieldNames, $searchConfigHashRef) = handleJob($jobDataHref, $job->id);
+        $beanstalkEvents->put(
+            {
+                priority => 0,
+                data     => encode_json(
+                    {
+                        event        => $STARTED,
+                        submissionID => $jobDataHref->{submissionID},
+                        queueID      => $job->id,
+                    }
+                )
+            }
+        );
 
-  } catch {      
-    # Don't store the stack
-    $err = $_; #substr($_, 0, index($_, 'at'));
-  };
+        ( $err, $fieldNames, $searchConfigHashRef ) =
+          handleJob( $jobDataHref, $job->id );
 
-  if ($err) {
-    if(defined $verbose || defined $debug) {
-      say "job ". $job->id . " failed due to found error";
-      p $err;
     }
-
-    my $message;
-
-    if(ref $err eq 'Search::Elasticsearch::Error::Request') {
-      $message = $err->{vars}{body}{error}{reason};
-    }
-
-    my $data = {
-      event        => $FAILED,
-      reason       => $err,
-      queueID      => $job->id,
-      submissionID => $jobDataHref->{submissionID},
+    catch {
+        # Don't store the stack
+        $err = $_;    #substr($_, 0, index($_, 'at'));
     };
+
+    if ($err) {
+        if ( defined $verbose || defined $debug ) {
+            say "job " . $job->id . " failed due to found error";
+            p $err;
+        }
+
+        my $message;
+
+        if ( ref $err eq 'Search::Elasticsearch::Error::Request' ) {
+            $message = $err->{vars}{body}{error}{reason};
+        }
+
+        my $data = {
+            event        => $FAILED,
+            reason       => $err,
+            queueID      => $job->id,
+            submissionID => $jobDataHref->{submissionID},
+        };
+
+        $beanstalkEvents->put( { priority => 0, data => encode_json($data) } );
+
+        $job->bury;
+
+        next;
+    }
+
+    # Signal completion before completion actually occurs via delete
+    # To be conservative; since after delete message is lost
+    my $data = {
+        event        => $COMPLETED,
+        queueID      => $job->id,
+        submissionID => $jobDataHref->{submissionID},
+        results      => {
+            fieldNames  => $fieldNames,
+            indexConfig => $searchConfigHashRef,
+        }
+    };
+
+    if ( defined $debug ) {
+        say STDERR "putting completiong event";
+        p $data;
+    }
 
     $beanstalkEvents->put( { priority => 0, data => encode_json($data) } );
 
-    $job->bury; 
+    $job->delete();
 
-    next;
-  }
-
-  # Signal completion before completion actually occurs via delete
-  # To be conservative; since after delete message is lost
-  my $data = {
-    event        => $COMPLETED,
-    queueID      => $job->id,
-    submissionID => $jobDataHref->{submissionID},
-    results => {
-        fieldNames => $fieldNames,
-        indexConfig => $searchConfigHashRef,
-    }
-  };
- 
- if ( defined $debug ) {
-    say STDERR "putting completiong event";
-    p $data;
-  }
-
-  $beanstalkEvents->put( { priority => 0, data => encode_json($data) } );
-
-  $job->delete();
-
-  say "completed job with queue id " . $job->id;
+    say "completed job with queue id " . $job->id;
 }
 
 sub handleJob {
-  my $submittedJob = shift;
-  my $queueID = shift;
+    my $submittedJob = shift;
+    my $queueID      = shift;
 
-  my $failed;
+    my $failed;
 
-  my ($err, $inputHref) = coerceInputs($submittedJob);
+    my ( $err, $inputHref ) = coerceInputs($submittedJob);
 
-  if($err) {
-    say STDERR $err;
-    return ($err, undef);
-  }
+    if ($err) {
+        say STDERR $err;
+        return ( $err, undef );
+    }
 
-  my $log_name = join '.', 'index', 'indexName', $inputHref->{indexName}, 'log';
-  my $logPath = File::Spec->rel2abs( ".", $log_name );
+    my $log_name = join '.', 'index', 'indexName', $inputHref->{indexName},
+      'log';
+    my $logPath = File::Spec->rel2abs( ".", $log_name );
 
-  if($maxThreads) {
-    $inputHref->{max_threads} = $maxThreads;
-  }
+    if ($maxThreads) {
+        $inputHref->{max_threads} = $maxThreads;
+    }
 
-  $inputHref->{logPath} = $logPath;
-  $inputHref->{verbose} = $verbose;
-  $inputHref->{debug} = $debug;
-  $inputHref->{publisher} = {
-      server      => $conf->{beanstalkd}{addresses}[0],
-      queue       => $queueConfig->{events},
-      messageBase => {
-        event        => $PROGRESS,
-        queueID      => $queueID,
-        submissionID => $submittedJob->{submissionID},
-        data         => undef,
-      }
+    $inputHref->{logPath}   = $logPath;
+    $inputHref->{verbose}   = $verbose;
+    $inputHref->{debug}     = $debug;
+    $inputHref->{publisher} = {
+        server      => $conf->{beanstalkd}{addresses}[0],
+        queue       => $queueConfig->{events},
+        messageBase => {
+            event        => $PROGRESS,
+            queueID      => $queueID,
+            submissionID => $submittedJob->{submissionID},
+            data         => undef,
+        }
     };
 
-  if(defined $verbose || defined $debug) {
-    say "in handle job, jobData is";
-    p $submittedJob;
-    say "writing beanstalk index queue log file here: $logPath";
-  }
+    if ( defined $verbose || defined $debug ) {
+        say "in handle job, jobData is";
+        p $submittedJob;
+        say "writing beanstalk index queue log file here: $logPath";
+    }
 
-  # create the annotator
-  my $indexer = SeqElastic->new($inputHref);
+    # create the annotator
+    my $indexer = SeqElastic->new($inputHref);
 
-  return $indexer->go;
+    return $indexer->go;
 }
 
 #Here we may wish to read a json or yaml file containing argument mappings
 sub coerceInputs {
-  my $jobDetailsHref = shift;
+    my $jobDetailsHref = shift;
 
-  my %return;
-  for my $fieldName (@requiredJobFields) {
-    if(!defined $jobDetailsHref->{$fieldName}) {
-      say STDERR "$fieldName required";
-      return ("$fieldName required", undef);
+    my %return;
+    for my $fieldName (@requiredJobFields) {
+        if ( !defined $jobDetailsHref->{$fieldName} ) {
+            say STDERR "$fieldName required";
+            return ( "$fieldName required", undef );
+        }
+
+        $return{$fieldName} = $jobDetailsHref->{$fieldName};
     }
 
-    $return{$fieldName} = $jobDetailsHref->{$fieldName};
-  }
+    $return{indexConfig} = LoadFile(
+        $configPathBaseDir . $jobDetailsHref->{assembly} . '.mapping.yml' );
+    %return = ( %return, %$connectionConfig );
 
-  $return{indexConfig} = LoadFile($configPathBaseDir . $jobDetailsHref->{assembly} . '.mapping.yml');
-  %return = (%return, %$connectionConfig);
-
-  return (undef, \%return);
+    return ( undef, \%return );
 }
 
 sub getConfigFilePath {
-  my $assembly = shift;
+    my $assembly = shift;
 
-  if ( exists $configFilePathHref->{$assembly} ) {
-    return $configFilePathHref->{$assembly};
-  }
-  else {
-    my @maybePath = glob( $configPathBaseDir . $assembly . ".y*ml" );
-    if ( scalar @maybePath ) {
-      if ( scalar @maybePath > 1 ) {
-        #should log
-        say "\n\nMore than 1 config path found, choosing first";
-      }
-
-      return $maybePath[0];
+    if ( exists $configFilePathHref->{$assembly} ) {
+        return $configFilePathHref->{$assembly};
     }
+    else {
+        my @maybePath = glob( $configPathBaseDir . $assembly . ".y*ml" );
+        if ( scalar @maybePath ) {
+            if ( scalar @maybePath > 1 ) {
 
-    die "\n\nNo config path found for the assembly $assembly. Exiting\n\n";
-    #throws the error
-    #should log here
-  }
+                #should log
+                say "\n\nMore than 1 config path found, choosing first";
+            }
+
+            return $maybePath[0];
+        }
+
+        die "\n\nNo config path found for the assembly $assembly. Exiting\n\n";
+
+        #throws the error
+        #should log here
+    }
 }
 1;
--- perl/bin/bystro-server.pl
+++ perl/bin/bystro-server.pl
@@ -30,17 +30,19 @@ use Path::Tiny qw/path/;
 my ( $verbose, $queueConfigPath, $connectionConfigPath, $maxThreads, $debug );
 
 GetOptions(
-  'v|verbose=i'          => \$verbose,
-  'd|debug'              => \$debug,
-  'q|queueConfig=s'      => \$queueConfigPath,
-  'c|connectionConfig=s' => \$connectionConfigPath,
-  'maxThreads=i'         => \$maxThreads,
+    'v|verbose=i'          => \$verbose,
+    'd|debug'              => \$debug,
+    'q|queueConfig=s'      => \$queueConfigPath,
+    'c|connectionConfig=s' => \$connectionConfigPath,
+    'maxThreads=i'         => \$maxThreads,
 );
 
-if ( !($queueConfigPath && $connectionConfigPath) ) {
-  # Generate a help strings that shows the arguments
-  say "\nUsage: perl $0 -q <queueConfigPath> -c <connectionConfigPath> --maxThreads <maxThreads>\n";
-  exit 1;
+if ( !( $queueConfigPath && $connectionConfigPath ) ) {
+
+    # Generate a help strings that shows the arguments
+    say
+"\nUsage: perl $0 -q <queueConfigPath> -c <connectionConfigPath> --maxThreads <maxThreads>\n";
+    exit 1;
 }
 
 my $type = 'annotation';
@@ -54,8 +56,8 @@ my $conf = LoadFile($queueConfigPath);
 
 # The properties that we accept from the worker caller
 my %requiredForAll = (
-  output_file_base => 'outputBasePath',
-  assembly         => 'assembly',
+    output_file_base => 'outputBasePath',
+    assembly         => 'assembly',
 );
 
 my $requiredForType = { input_file => 'inputFilePath' };
@@ -68,247 +70,252 @@ my $configFilePathHref = {};
 my $queueConfig = $conf->{beanstalkd}{tubes}{$type};
 
 if ( !$queueConfig ) {
-  die "Queue config format not recognized. Options are "
-    . ( join( ', ', @{ keys %{ $conf->{beanstalkd}{tubes} } } ) );
+    die "Queue config format not recognized. Options are "
+      . ( join( ', ', @{ keys %{ $conf->{beanstalkd}{tubes} } } ) );
 }
 
 my $beanstalk = Beanstalk::Client->new(
-  {
-    server          => $conf->{beanstalkd}{addresses}[0],
-    default_tube    => $queueConfig->{submission},
-    connect_timeout => 1,
-    encoder         => sub { encode_json( \@_ ) },
-    decoder         => sub { @{ decode_json(shift) } },
-  }
+    {
+        server          => $conf->{beanstalkd}{addresses}[0],
+        default_tube    => $queueConfig->{submission},
+        connect_timeout => 1,
+        encoder         => sub { encode_json( \@_ ) },
+        decoder         => sub { @{ decode_json(shift) } },
+    }
 );
 
 my $beanstalkEvents = Beanstalk::Client->new(
-  {
-    server          => $conf->{beanstalkd}{addresses}[0],
-    default_tube    => $queueConfig->{events},
-    connect_timeout => 1,
-    encoder         => sub { encode_json( \@_ ) },
-    decoder         => sub { @{ decode_json(shift) } },
-  }
+    {
+        server          => $conf->{beanstalkd}{addresses}[0],
+        default_tube    => $queueConfig->{events},
+        connect_timeout => 1,
+        encoder         => sub { encode_json( \@_ ) },
+        decoder         => sub { @{ decode_json(shift) } },
+    }
 );
 
 while ( my $job = $beanstalk->reserve ) {
-  # Parallel ForkManager used only to throttle number of jobs run in parallel
-  # cannot use run_on_finish with blocking reserves, use try catch instead
-  # Also using forks helps clean up leaked memory from LMDB_File
-  # Unfortunately, parallel fork manager doesn't play nicely with try tiny
-  # prevents anything within the try from executing
 
-  my $jobDataHref;
-  my ( $err, $outputFileNamesHashRef );
+    # Parallel ForkManager used only to throttle number of jobs run in parallel
+    # cannot use run_on_finish with blocking reserves, use try catch instead
+    # Also using forks helps clean up leaked memory from LMDB_File
+    # Unfortunately, parallel fork manager doesn't play nicely with try tiny
+    # prevents anything within the try from executing
 
-  try {
-    $jobDataHref = decode_json( $job->data );
+    my $jobDataHref;
+    my ( $err, $outputFileNamesHashRef );
 
-    if ( defined $debug ) {
-      say "Reserved job with id " . $job->id . " which contains:";
-      p $jobDataHref;
+    try {
+        $jobDataHref = decode_json( $job->data );
 
-      my $stats = $job->stats();
-      say "stats are";
-      p $stats;
+        if ( defined $debug ) {
+            say "Reserved job with id " . $job->id . " which contains:";
+            p $jobDataHref;
+
+            my $stats = $job->stats();
+            say "stats are";
+            p $stats;
+        }
+
+        ( $err, my $inputHref ) = coerceInputs( $jobDataHref, $job->id );
+
+        if ($err) {
+            die $err;
+        }
+
+        my $configData = LoadFile( $inputHref->{config} );
+
+        # Hide the server paths in the config we send back;
+        # Those represent a security concern
+        $configData->{files_dir} = 'hidden';
+
+        if ( $configData->{temp_dir} ) {
+            $configData->{temp_dir} = 'hidden';
+        }
+
+        $configData->{database_dir} = 'hidden';
+
+        my $trackConfig;
+        if ( ref $configData->{tracks} eq 'ARRAY' ) {
+            $trackConfig = $configData->{tracks};
+        }
+        else {
+            # New version
+            $trackConfig = $configData->{tracks}{tracks};
+        }
+
+        for my $track (@$trackConfig) {
+
+            # Strip local_files of their directory names, for security reasons
+            $track->{local_files} =
+              [ map { !$_ ? "" : path($_)->basename }
+                  @{ $track->{local_files} } ];
+        }
+
+        $beanstalkEvents->put(
+            {
+                priority => 0,
+                data     => encode_json(
+                    {
+                        event        => $STARTED,
+                        jobConfig    => $configData,
+                        submissionID => $jobDataHref->{submissionID},
+                        queueID      => $job->id,
+                    }
+                )
+            }
+        );
+
+        if ($debug) {
+            say STDERR "job " . $job->id . " starting with inputHref:";
+            p $inputHref;
+        }
+
+        my $annotate_instance = Seq->new_with_config($inputHref);
+
+        ( $err, $outputFileNamesHashRef ) = $annotate_instance->annotate();
+
+        if ( defined $debug ) {
+            p $err;
+        }
     }
-
-    ( $err, my $inputHref ) = coerceInputs( $jobDataHref, $job->id );
-
-    if ($err) {
-      die $err;
-    }
-
-    my $configData = LoadFile( $inputHref->{config} );
-
-    # Hide the server paths in the config we send back;
-    # Those represent a security concern
-    $configData->{files_dir} = 'hidden';
-
-    if ( $configData->{temp_dir} ) {
-      $configData->{temp_dir} = 'hidden';
-    }
-
-    $configData->{database_dir} = 'hidden';
-
-    my $trackConfig;
-    if ( ref $configData->{tracks} eq 'ARRAY' ) {
-      $trackConfig = $configData->{tracks};
-    }
-    else {
-      # New version
-      $trackConfig = $configData->{tracks}{tracks};
-    }
-
-    for my $track (@$trackConfig) {
-      # Strip local_files of their directory names, for security reasons
-      $track->{local_files} =
-        [ map { !$_ ? "" : path($_)->basename } @{ $track->{local_files} } ];
-    }
-
-    $beanstalkEvents->put(
-      {
-        priority => 0,
-        data     => encode_json(
-          {
-            event        => $STARTED,
-            jobConfig    => $configData,
-            submissionID => $jobDataHref->{submissionID},
-            queueID      => $job->id,
-          }
-        )
-      }
-    );
-
-    if ($debug) {
-      say STDERR "job " . $job->id . " starting with inputHref:";
-      p $inputHref;
-    }
-
-    my $annotate_instance = Seq->new_with_config($inputHref);
-
-    ( $err, $outputFileNamesHashRef ) = $annotate_instance->annotate();
-
-    if ( defined $debug ) {
-      p $err;
-    }
-  }
-  catch {
-    $err = $_;
-  };
-
-  if ( defined $debug ) {
-    p $err;
-  }
-
-  if ($err) {
-    $err =~ s/\sat\s\w+\/\w+.*\sline\s\d+.*//;
-
-    say "job " . $job->id . " failed";
-
-    my $data = {
-      event        => $FAILED,
-      reason       => $err,
-      queueID      => $job->id,
-      submissionID => $jobDataHref->{submissionID},
+    catch {
+        $err = $_;
     };
 
+    if ( defined $debug ) {
+        p $err;
+    }
+
+    if ($err) {
+        $err =~ s/\sat\s\w+\/\w+.*\sline\s\d+.*//;
+
+        say "job " . $job->id . " failed";
+
+        my $data = {
+            event        => $FAILED,
+            reason       => $err,
+            queueID      => $job->id,
+            submissionID => $jobDataHref->{submissionID},
+        };
+
+        $beanstalkEvents->put( { priority => 0, data => encode_json($data) } );
+
+        $job->delete();
+
+        if ( $beanstalkEvents->error && defined $debug ) {
+            say STDERR "Beanstalkd last error:";
+            p $beanstalkEvents->error;
+        }
+
+        next;
+    }
+
+    my $data = {
+        event        => $COMPLETED,
+        queueID      => $job->id,
+        submissionID => $jobDataHref->{submissionID},
+        results      => { outputFileNames => $outputFileNamesHashRef, }
+    };
+
+    if ( defined $debug ) {
+        say STDERR "putting completiong event";
+        p $data;
+    }
+
+    # Signal completion before completion actually occurs via delete
+    # To be conservative; since after delete message is lost
     $beanstalkEvents->put( { priority => 0, data => encode_json($data) } );
 
     $job->delete();
 
     if ( $beanstalkEvents->error && defined $debug ) {
-      say STDERR "Beanstalkd last error:";
-      p $beanstalkEvents->error;
+        say "Beanstalkd last error:";
+        p $beanstalkEvents->error;
     }
 
-    next;
-  }
-
-  my $data = {
-    event        => $COMPLETED,
-    queueID      => $job->id,
-    submissionID => $jobDataHref->{submissionID},
-    results      => { outputFileNames => $outputFileNamesHashRef, }
-  };
-
-  if ( defined $debug ) {
-    say STDERR "putting completiong event";
-    p $data;
-  }
-
-  # Signal completion before completion actually occurs via delete
-  # To be conservative; since after delete message is lost
-  $beanstalkEvents->put( { priority => 0, data => encode_json($data) } );
-
-  $job->delete();
-
-  if ( $beanstalkEvents->error && defined $debug ) {
-    say "Beanstalkd last error:";
-    p $beanstalkEvents->error;
-  }
-
-  say "completed job with queue id " . $job->id;
+    say "completed job with queue id " . $job->id;
 }
 
 sub coerceInputs {
-  my $jobDetailsHref = shift;
-  my $queueId        = shift;
+    my $jobDetailsHref = shift;
+    my $queueId        = shift;
 
-  my %args;
-  my $err;
+    my %args;
+    my $err;
 
-  my %jobSpecificArgs;
-  for my $key ( keys %requiredForAll ) {
-    if ( !defined $jobDetailsHref->{ $requiredForAll{$key} } ) {
-      $err = "Missing required key: $key in job message";
-      return ( $err, undef );
+    my %jobSpecificArgs;
+    for my $key ( keys %requiredForAll ) {
+        if ( !defined $jobDetailsHref->{ $requiredForAll{$key} } ) {
+            $err = "Missing required key: $key in job message";
+            return ( $err, undef );
+        }
+
+        $jobSpecificArgs{$key} = $jobDetailsHref->{ $requiredForAll{$key} };
     }
 
-    $jobSpecificArgs{$key} = $jobDetailsHref->{ $requiredForAll{$key} };
-  }
+    for my $key ( keys %$requiredForType ) {
+        if ( !defined $jobDetailsHref->{ $requiredForType->{$key} } ) {
+            $err = "Missing required key: $key in job message";
+            return ( $err, undef );
+        }
 
-  for my $key ( keys %$requiredForType ) {
-    if ( !defined $jobDetailsHref->{ $requiredForType->{$key} } ) {
-      $err = "Missing required key: $key in job message";
-      return ( $err, undef );
+        $jobSpecificArgs{$key} = $jobDetailsHref->{ $requiredForType->{$key} };
     }
 
-    $jobSpecificArgs{$key} = $jobDetailsHref->{ $requiredForType->{$key} };
-  }
+    my $configFilePath = getConfigFilePath( $jobSpecificArgs{assembly} );
 
-  my $configFilePath = getConfigFilePath( $jobSpecificArgs{assembly} );
+    if ( !$configFilePath ) {
+        $err =
+"Assembly $jobSpecificArgs{assembly} doesn't have corresponding config file";
+        return ( $err, undef );
+    }
 
-  if ( !$configFilePath ) {
-    $err = "Assembly $jobSpecificArgs{assembly} doesn't have corresponding config file";
-    return ( $err, undef );
-  }
+    my %commmonArgs = (
+        config    => $configFilePath,
+        publisher => {
+            server      => $conf->{beanstalkd}{addresses}[0],
+            queue       => $queueConfig->{events},
+            messageBase => {
+                event        => $PROGRESS,
+                queueID      => $queueId,
+                submissionID => $jobDetailsHref->{submissionID},
+                data         => undef,
+            }
+        },
+        compress       => 1,
+        archive        => 1,
+        verbose        => $verbose,
+        run_statistics => 1,
+    );
 
-  my %commmonArgs = (
-    config    => $configFilePath,
-    publisher => {
-      server      => $conf->{beanstalkd}{addresses}[0],
-      queue       => $queueConfig->{events},
-      messageBase => {
-        event        => $PROGRESS,
-        queueID      => $queueId,
-        submissionID => $jobDetailsHref->{submissionID},
-        data         => undef,
-      }
-    },
-    compress       => 1,
-    archive        => 1,
-    verbose        => $verbose,
-    run_statistics => 1,
-  );
+    if ($maxThreads) {
+        $commmonArgs{maxThreads} = $maxThreads;
+    }
 
-  if ($maxThreads) {
-    $commmonArgs{maxThreads} = $maxThreads;
-  }
+    my %combined = ( %commmonArgs, %jobSpecificArgs );
 
-  my %combined = ( %commmonArgs, %jobSpecificArgs );
-
-  return ( undef, \%combined );
+    return ( undef, \%combined );
 }
 
 sub getConfigFilePath {
-  my $assembly = shift;
+    my $assembly = shift;
 
-  if ( exists $configFilePathHref->{$assembly} ) {
-    return $configFilePathHref->{$assembly};
-  }
-  else {
-    my @maybePath = glob( $configPathBaseDir . $assembly . ".y*ml" );
-    if ( scalar @maybePath ) {
-      if ( scalar @maybePath > 1 ) {
-        #should log
-        say "\n\nMore than 1 config path found, choosing first";
-      }
-
-      return $maybePath[0];
+    if ( exists $configFilePathHref->{$assembly} ) {
+        return $configFilePathHref->{$assembly};
     }
+    else {
+        my @maybePath = glob( $configPathBaseDir . $assembly . ".y*ml" );
+        if ( scalar @maybePath ) {
+            if ( scalar @maybePath > 1 ) {
 
-    die "\n\nNo config path found for the assembly $assembly. Exiting\n\n";
-  }
+                #should log
+                say "\n\nMore than 1 config path found, choosing first";
+            }
+
+            return $maybePath[0];
+        }
+
+        die "\n\nNo config path found for the assembly $assembly. Exiting\n\n";
+    }
 }
