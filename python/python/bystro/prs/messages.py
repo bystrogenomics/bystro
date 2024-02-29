bf0881016a85518f3f4467e4be80a6b578ed4f70
Alex Kotlar <akotlar@bu.edu>
2024-02-29 00:56:05 +0000

    Don't attempt to load conf_dir, ensure camel case for incoming messages and serialized values

--- python/python/bystro/prs/messages.py
+++ python/python/bystro/prs/messages.py
@@ -1,7 +1,7 @@
 from bystro.beanstalkd.messages import BaseMessage, CompletedJobMessage, SubmittedJobMessage, Struct
 
 
-class PRSJobData(BaseMessage, frozen=True, forbid_unknown_fields=True):
+class PRSJobData(BaseMessage, frozen=True, forbid_unknown_fields=True, rename="camel"):
     """Data for PRS jobs received from beanstalkd"""
 
     dosage_matrix_path: str
@@ -9,22 +9,24 @@ class PRSJobData(BaseMessage, frozen=True, forbid_unknown_fields=True):
     assembly: str
 
 
-class PRSJobSubmitMessage(SubmittedJobMessage, frozen=True, kw_only=True):
+class PRSJobSubmitMessage(SubmittedJobMessage, frozen=True, kw_only=True, rename="camel"):
     """
     The acknowledgement message to be sent back through beanstalkd
     after PRS job is picked up for processing
     """
 
-    jobConfig: dict
+    job_config: dict
 
 
-class PRSJobResult(Struct, frozen=True, forbid_unknown_fields=True):
+class PRSJobResult(Struct, frozen=True, forbid_unknown_fields=True, rename="camel"):
     """The contents of the PRS result object to be sent back through beanstalkd"""
 
     prs_scores_path: str
 
 
-class PRSJobResultMessage(CompletedJobMessage, frozen=True, forbid_unknown_fields=True, kw_only=True):
+class PRSJobResultMessage(
+    CompletedJobMessage, frozen=True, forbid_unknown_fields=True, kw_only=True, rename="camel"
+):
     """
     Results to be sent back through beanstalkd after PRS job is completed
     prs_scores_path: str
