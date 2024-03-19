3e8146921ecc93ebe8ca27ac21d1c8ddcc8e8a6c
Alex Kotlar <akotlar@bu.edu>
2024-03-19 18:12:59 -0400

    fixes

--- python/python/bystro/proteomics/somascan.py
+++ python/python/bystro/proteomics/somascan.py
@@ -1,6 +1,6 @@
 """Load and prep fragpipe tandem mass Tag datasets."""
 
-import canopy
+import canopy  # type: ignore
 
 from msgspec import Struct
 
@@ -9,7 +9,7 @@ class SomascanDataset(Struct):
     """Represent a Fragpipe Tandem Mass Tag dataset."""
 
     adat: canopy.Adat
-    annotations: canopy.Annotations | None
+    annotations: canopy.Annotations | None = None
 
     @staticmethod
     def from_paths(adat_path: str, annotations_path: str | None = None) -> "SomascanDataset":
@@ -23,3 +23,17 @@ class SomascanDataset(Struct):
             annotations = canopy.read_annotations(annotations_path)
 
         return SomascanDataset(adat, annotations)
+
+    def __post_init__(self) -> None:
+        if not isinstance(self.adat, canopy.Adat):
+            raise ValueError(
+                f"`adat` argument to SomascanDataset must be a canopy.Adat object, not {type(self.adat)}"
+            )
+
+        if self.annotations and not isinstance(self.annotations, canopy.Annotations):
+            raise ValueError(
+                (
+                    "`annotations` argument to SomascanDataset must be a canopy.Annotations object"
+                    f", not {type(self.annotations)}"
+                )
+            )
