d48cf12ec36adf6af40078fae9ea83c104ab605e
Alex Kotlar <akotlar@bu.edu>
2024-04-05 01:51:28 -0400

    Add fast dosage matrix filtering function

---
+++ go/cmd/dosage-filter/main.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/apache/arrow/go/v14/arrow/array"
+	"github.com/apache/arrow/go/v14/arrow/ipc"
+	"github.com/apache/arrow/go/v14/arrow/memory"
+	bystroArrow "github.com/bystrogenomics/bystro-vcf/arrow"
+)
+
+var WRITE_CHUNK_SIZE = 1000
+
+type CLIArgs struct {
+	inputPath           string
+	outputPath          string
+	beanstalkConfigPath string
+	jobSubmissionID     string
+	lociPath            string
+	noBeanstalkd        bool
+	progressFrequency   int
+}
+
+func setup(args []string) *CLIArgs {
+	cliargs := &CLIArgs{}
+	flag.StringVar(&cliargs.inputPath, "input", "", "The path to the input Arrow IPC/Feather file")
+	flag.StringVar(&cliargs.inputPath, "in", "", "The path to the input Arrow IPC/Feather file")
+
+	flag.StringVar(&cliargs.lociPath, "loci", "", "The path to a file containing loci to filter, one locus per line")
+
+	flag.StringVar(&cliargs.outputPath, "output", "", "The path to the output Arrow IPC/Feather file")
+	flag.StringVar(&cliargs.outputPath, "out", "", "The path to the output Arrow IPC/Feather file")
+
+	flag.StringVar(&cliargs.beanstalkConfigPath, "queue-config", "", "The path to the Beanstalkd queue connection config (e.g. config/beanstalk.yml)")
+	flag.StringVar(&cliargs.beanstalkConfigPath, "q", "", "The path to the Beanstalkd queue connection config (short form)")
+	flag.BoolVar(&cliargs.noBeanstalkd, "no-queue", true, "Disable beanstalkd progress events")
+	flag.BoolVar(&cliargs.noBeanstalkd, "n", true, "Disable beanstalkd progress events (short form)")
+
+	flag.IntVar(&cliargs.progressFrequency, "progress-frequency", 5e3, "Print progress every N variants processed")
+	flag.IntVar(&cliargs.progressFrequency, "p", 5e3, "Print progress every N variants processed (short form)")
+
+	flag.StringVar(&cliargs.jobSubmissionID, "job-submission-id", "", "The job submission ID")
+	flag.StringVar(&cliargs.jobSubmissionID, "j", "", "The job submission ID (short form)")
+
+	a := os.Args[1:]
+	if args != nil {
+		a = args
+	}
+
+	flag.CommandLine.Parse(a)
+
+	if err := validateArgs(cliargs); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	return cliargs
+}
+
+func validateArgs(args *CLIArgs) error {
+	var missing []string
+	v := *args
+	if v.inputPath == "" {
+		missing = append(missing, "input")
+	}
+	if v.outputPath == "" {
+		missing = append(missing, "output")
+	}
+	if v.lociPath == "" {
+		missing = append(missing, "os-connection-config-path")
+	}
+	if v.beanstalkConfigPath == "" {
+		if !v.noBeanstalkd {
+			missing = append(missing, "beanstalk-config-path")
+		}
+	}
+
+	if v.jobSubmissionID == "" {
+		if !v.noBeanstalkd {
+			missing = append(missing, "job-submission-id")
+		}
+	}
+
+	if len(missing) > 0 {
+		return fmt.Errorf("missing required arguments: %s", strings.Join(missing, ", "))
+	}
+
+	return nil
+}
+
+func readLociFile(filePath string) (map[string]bool, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	loci := make(map[string]bool)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		loci[scanner.Text()] = true
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return loci, nil
+}
+
+func processRecordAt(fr *ipc.FileReader, loci map[string]bool, arrowWriter *bystroArrow.ArrowWriter, queue chan int, complete chan bool) {
+	pool := memory.NewGoAllocator()
+	builder := array.NewRecordBuilder(pool, arrowWriter.Schema)
+	defer builder.Release()
+
+	// Fetch the specific record by its index
+	rowsAccumulated := 0
+	for index := range queue {
+		// start, end := startEnd[0], startEnd[1]
+
+		// for index := start; index < end; index++ {
+
+		record, err := fr.RecordAt(index)
+		if err != nil {
+			log.Fatalf("Failed to read record at %d: %v", index, err)
+		}
+
+		// Here you would process the record as needed
+		// log.Printf("Processing record at %d with %d rows", index, record.NumRows())
+
+		// Correcting the type for the "locus" column to use *array.String
+		locusCol := record.Column(0).(*array.String)
+
+		for j := 0; j < int(record.NumRows()); j++ {
+			locusValue := locusCol.Value(j)
+			if _, exists := loci[locusValue]; exists {
+				rowsAccumulated += 1
+
+				// Fill the builder with values from the original columns, filtering by selectedIndices
+				for colIdx := 0; colIdx < int(record.NumCols()); colIdx++ {
+					column := record.Column(colIdx)
+					switch column := column.(type) {
+					case *array.String:
+						builder.Field(colIdx).(*array.StringBuilder).Append(column.Value(j))
+					case *array.Uint8:
+						builder.Field(colIdx).(*array.Uint8Builder).Append(column.Value(j))
+					case *array.Uint16:
+						builder.Field(colIdx).(*array.Uint16Builder).Append(column.Value(j))
+					case *array.Uint32:
+						builder.Field(colIdx).(*array.Uint32Builder).Append(column.Value(j))
+					case *array.Uint64:
+						builder.Field(colIdx).(*array.Uint64Builder).Append(column.Value(j))
+					case *array.Int8:
+						builder.Field(colIdx).(*array.Int8Builder).Append(column.Value(j))
+					case *array.Int16:
+						builder.Field(colIdx).(*array.Int16Builder).Append(column.Value(j))
+					case *array.Int32:
+						builder.Field(colIdx).(*array.Int32Builder).Append(column.Value(j))
+					case *array.Int64:
+						builder.Field(colIdx).(*array.Int64Builder).Append(column.Value(j))
+					case *array.Float16:
+						builder.Field(colIdx).(*array.Float16Builder).Append(column.Value(j))
+					case *array.Float32:
+						builder.Field(colIdx).(*array.Float32Builder).Append(column.Value(j))
+					case *array.Float64:
+						builder.Field(colIdx).(*array.Float64Builder).Append(column.Value(j))
+					default:
+						log.Fatalf("Unsupported column type: %T", column)
+					}
+				}
+			}
+		}
+
+		record.Release()
+
+		if rowsAccumulated >= WRITE_CHUNK_SIZE {
+			// Create a new record from the row
+			filteredRecord := builder.NewRecord()
+
+			// Write the new record to the output file
+			if err := arrowWriter.WriteChunk(filteredRecord); err != nil {
+				log.Fatal(err)
+			}
+
+			filteredRecord.Release()
+			builder.Release()
+
+			builder = array.NewRecordBuilder(pool, arrowWriter.Schema)
+
+			rowsAccumulated = 0
+		}
+		// }
+	}
+
+	if rowsAccumulated >= 0 {
+		// Create a new record from the row
+		filteredRecord := builder.NewRecord()
+
+		// Write the new record to the output file
+		if err := arrowWriter.WriteChunk(filteredRecord); err != nil {
+			log.Fatal(err)
+		}
+
+		filteredRecord.Release()
+		builder.Release()
+		rowsAccumulated = 0
+	}
+
+	complete <- true
+}
+
+func main() {
+	cliargs := setup(nil)
+
+	loci, err := readLociFile(cliargs.lociPath)
+	if err != nil {
+		log.Fatalf("Could not read loci file: %v", err)
+	}
+
+	// Open Arrow IPC file
+	file, err := os.Open(cliargs.inputPath)
+	if err != nil {
+		log.Fatalf("Failed to open IPC file: %v", err)
+	}
+	defer file.Close()
+
+	pool := memory.NewGoAllocator()
+	fr, err := ipc.NewFileReader(file, ipc.WithAllocator(pool))
+	if err != nil {
+		log.Fatalf("Failed to create IPC file reader: %v", err)
+	}
+
+	schema := fr.Schema()
+
+	outFile, err := os.Create(cliargs.outputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	arrowWriter, err := bystroArrow.NewArrowIPCFileWriterWithSchema(outFile, schema, ipc.WithZstd())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer arrowWriter.Close()
+
+	totalRecords := fr.NumRecords()
+	numWorkers := 32
+	recordsPerWorker := totalRecords / numWorkers
+
+	// var wg sync.WaitGroup
+	workQueue := make(chan int, 16)
+	complete := make(chan bool)
+
+	// Spawn threads
+	for i := 0; i < numWorkers; i++ {
+		go processRecordAt(fr, loci, arrowWriter, workQueue, complete)
+	}
+
+	for i := 0; i < numWorkers; i++ {
+		start := i * recordsPerWorker
+		end := start + recordsPerWorker
+		if i == numWorkers-1 {
+			// Ensure the last worker picks up any remaining records
+			end = totalRecords
+		}
+		for j := start; j < end; j++ {
+			workQueue <- j
+		}
+	}
+
+	close(workQueue)
+
+	for i := 0; i < numWorkers; i++ {
+		<-complete
+	}
+
+}
--- go/go.mod
+++ go/go.mod
@@ -7,24 +7,25 @@ require (
 	github.com/beanstalkd/go-beanstalk v0.2.0
 	github.com/biogo/hts v1.4.4
 	github.com/bytedance/sonic v1.10.2
-	github.com/klauspost/compress v1.17.3
+	github.com/klauspost/compress v1.17.4
 	github.com/klauspost/pgzip v1.2.6
 	github.com/opensearch-project/opensearch-go/v2 v2.3.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/bystrogenomics/bystro-vcf v0.0.0-20240402234936-cb7a8a568d10 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/flatbuffers v23.5.26+incompatible // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
-	github.com/pierrec/lz4/v4 v4.1.18 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
 	golang.org/x/arch v0.6.0 // indirect
-	golang.org/x/mod v0.13.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/tools v0.14.0 // indirect
-	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 )
