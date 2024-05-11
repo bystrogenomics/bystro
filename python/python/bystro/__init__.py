803cd603fad5b60c8210003d0a886e5401f25661
Alex Kotlar <akotlar@bu.edu>
2024-05-11 05:33:41 -0400

    attempt to fix __init__.py

--- python/python/bystro/__init__.py
+++ python/python/bystro/__init__.py
@@ -1,4 +1,4 @@
-from .bystro import *  # type: ignore # noqa: F403
-from .bystro import __all__
-
-__all__ = __all__ + ["search"]  # noqa: F405
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
