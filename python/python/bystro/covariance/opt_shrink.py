4dc89cdd0518f7ff3f38930e2246ec1165ed6912
Ilha Hwang <legitilhah@gmail.com>
2024-05-10 19:26:32 -0600

    Add typing to opt_shrink

--- python/python/bystro/covariance/opt_shrink.py
+++ python/python/bystro/covariance/opt_shrink.py
@@ -1,8 +1,8 @@
 import numpy as np
-import numpy.linalg as la
+from numpy.linalg import inv
 
 
-def opt_shrink(singular_values, r):
+def opt_shrink(singular_values: np.ndarray, r: int) -> np.ndarray:
     """
     Perform operator norm optimal shrinkage on data singular values.
 
@@ -11,28 +11,27 @@ def opt_shrink(singular_values, r):
     singular_values : array-like
         A vector of data eigenvalues, obtained by running eig on the p-by-p sample covariance matrix
         corresponding to a dataset of n samples in dimension p.
-    r : float
+    r : int
         Estimate of the effective rank of the latent low-rank signal matrix
-        singular values
+        singular values.
 
     Returns
     -------
-    shrunk_values : array
+    shrunk_values : np.ndarray
         The vector of singular values after performing optimal shrinkage.
-
     """
-    Sigma2_r = np.diag(singular_values[r:]) ** 2
-    n = len(Sigma2_r)
+    Sigma2_r = np.square(singular_values[r:])
 
     shrunk_values = []
+    n = len(Sigma2_r)
+
     for z_k in singular_values[:r]:
-        resolvent = la.inv(z_k**2 * np.eye(n) - Sigma2_r)
-        D = 1 / n**2 * np.trace(z_k * resolvent) ** 2
-        D_prime = (
-            2
-            / n**2
-            * (np.trace(z_k * resolvent) * np.trace(-2 * z_k**2 * resolvent**2 + resolvent))
-        )
+        resolvent = inv(z_k**2 * np.eye(n) - np.diag(Sigma2_r))
+        trace_resolvent = np.trace(z_k * resolvent)
+        trace_resolvent_square = np.trace(-2 * z_k**2 * resolvent @ resolvent + resolvent)
+        D = (trace_resolvent / n) ** 2
+        D_prime = 2 / n * (trace_resolvent * trace_resolvent_square)
         w_opt = -2 * D / D_prime
         shrunk_values.append(w_opt)
+
     return np.array(shrunk_values)
