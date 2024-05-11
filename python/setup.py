b1e48189d7a10d0d67dc8161a4d8a141eed69ef8
Alex Kotlar <akotlar@bu.edu>
2024-05-10 23:19:48 -0400

    fix

--- python/setup.py
+++ python/setup.py
@@ -9,8 +9,5 @@ setup(
         build_dir="build",
         compiler_directives={"language_level": "3"},
     ),
-    zip_safe=False,
-    install_requires=[
-        'Canopy @ git+https://github.com/bystrogenomics/canopy.git@f6d3031494a692db040492fa1dead5faf19fef5a'
-    ]
+    zip_safe=False
 )
