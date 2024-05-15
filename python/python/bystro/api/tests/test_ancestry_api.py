9629c407cb2bd300cee7f52c087a167220cb38ba
Alex Kotlar <akotlar@bu.edu>
2024-05-15 00:04:50 -0400

    add back integration tag

--- python/python/bystro/api/tests/test_ancestry_api.py
+++ python/python/bystro/api/tests/test_ancestry_api.py
@@ -17,6 +17,7 @@ from bystro.ancestry.tests.test_inference import (
 from bystro.api.ancestry import calculate_ancestry_scores
 
 
+@pytest.mark.integration("Requires bystro-vcf to be installed as well as AWS credentials.")
 def test_calculate_ancestry_scores_happy_path(mocker, tmpdir):
     mocker.patch(
         "bystro.ancestry.model.get_models_from_s3",
