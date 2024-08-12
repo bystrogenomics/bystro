b34a4c3b0b64ea837b6169b6113c57a68d78e706
Alex Kotlar <akotlar@bu.edu>
2024-08-11 22:40:23 -0400

    cleanup

--- perl/bin/bystro-build.pl
+++ perl/bin/bystro-build.pl
@@ -18,7 +18,7 @@ use Seq::Build;
 my (
   $yaml_config, $wantedType,      $wantedName, $verbose,
   $maxThreads,  $help,            $wantedChr,  $dryRunInsertions,
-  $logDir,      $metaOnly,        $debug,      $overwrite,
+  $logDir,      $debug,      $overwrite,
   $delete,      $regionTrackOnly, $skipCompletionCheck
 );
 
@@ -27,20 +27,19 @@ $debug = 0;
 # usage
 GetOptions(
   'c|config=s'                                => \$yaml_config,
-  't|type|wantedType=s'                       => \$wantedType,
-  'n|name|wantedName=s'                       => \$wantedName,
+  't|type=s'                                  => \$wantedType,
+  'n|name=s'                                  => \$wantedName,
   'v|verbose=i'                               => \$verbose,
   'h|help'                                    => \$help,
   'd|debug=i'                                 => \$debug,
   'o|overwrite'                               => \$overwrite,
-  'chr|wantedChr=s'                           => \$wantedChr,
+  'chr=s'                                     => \$wantedChr,
   'delete'                                    => \$delete,
   'build_region_track_only'                   => \$regionTrackOnly,
-  'skipCompletionCheck|skip_completion_check' => \$skipCompletionCheck,
+  'skip_completion_check'                     => \$skipCompletionCheck,
   'dry_run_insertions|dry|dryRun'             => \$dryRunInsertions,
   'log_dir=s'                                 => \$logDir,
-  'threads=i'                                 => \$maxThreads,
-  'meta_only'                                 => \$metaOnly,
+  'threads=i'                                 => \$maxThreads
 ) or pod2usage(2);
 
 if ($help) {
@@ -67,7 +66,6 @@ if ($debug) {
     dryRunInsertions        => !!$dryRunInsertions,
     logDir                  => $logDir,
     maxThreads              => $maxThreads,
-    metaOnly                => !!$metaOnly,
     verbose                 => $verbose,
   };
 
@@ -110,7 +108,6 @@ my $builder_options_href = {
   build_region_track_only => !!$regionTrackOnly,
   skipCompletionCheck     => !!$skipCompletionCheck,
   dryRun                  => !!$dryRunInsertions,
-  meta_only               => !!$metaOnly,
   verbose                 => $verbose,
 };
 
@@ -132,20 +129,19 @@ build_genome_assembly [options]
 
  Options:
    -c, --config                   YAML configuration file
-   -t, --type, --wantedType       Type of build (e.g., genome, conserv, transcript_db, gene_db, snp_db)
-   -n, --name, --wantedName       Name of the build
+   -t, --type                     Type of build (e.g., genome, conserv, transcript_db, gene_db, snp_db)
+   -n, --name                     Name of the build
    -v, --verbose                  Verbosity level
    -h, --help                     Display this help message
    -d, --debug                    Debug level (default: 0)
-   -o, --overwrite                Overwrite existing files
-   --chr, --wantedChr             Chromosome to build (if applicable)
-   --delete                       Delete mode
+   -o, --overwrite                For a given track, overwrite existing track values, rather than merging them
+   --chr                          Chromosome to build (if applicable)
+   --delete                       Delete the track instead of building it
    --build_region_track_only      Build region track only
-   --skipCompletionCheck          Skip completion check
-   --dry_run_insertions, --dry    Perform a dry run of insertions
+   --skip_completion_check        Skip completion check
+   --dry_run_insertions, --dry    Perform a dry run
    --log_dir                      Directory for log files
    --threads                      Number of threads to use
-   --meta_only                    Meta only mode
 
 =head1 DESCRIPTION
 
@@ -159,61 +155,57 @@ index of the genome and associated annotations in the MongoDB instance.
 
 =item B<-c>, B<--config>
 
-Config: A YAML genome assembly configuration file that specifies the various
+config: A YAML genome assembly configuration file that specifies the various
 tracks and data associated with the assembly. This is the same file that is
 used by the Seq Package to annotate VCF and SNP files.
 
-=item B<-t>, B<--type>, B<--wantedType>
+=item B<-t>, B<--type>
 
-Type: Build all tracks in the configuration file with `type: <this_type>``.
+type: Build all tracks in the configuration file with `type: <this_type>``.
 
-=item B<-n>, B<--name>, B<--wantedName>
+=item B<-n>, B<--name>
 
-Name: Build the track specified in the configuration file with `name: <this_name>``.
+name: Build the track specified in the configuration file with `name: <this_name>``.
 
-=item B<--chr>, B<--wantedChr>
+=item B<--chr>
 
-Wanted_chr: Chromosome to build, if building gene or SNP; will build all if not specified.
+chr: Chromosome to build, if building gene or SNP; will build all if not specified.
 
 =item B<-v>, B<--verbose>
 
-Verbose: Verbosity level (default: 0).
+verbose: Verbosity level (default: 0).
 
 =item B<-d>, B<--debug>
 
-Debug: Debug level (default: 0).
+debug: Debug level (default: 0).
 
 =item B<-o>, B<--overwrite>
 
-Overwrite: Overwrite existing files.
+overwrite: For a given track, overwrite existing track values, rather than merging them
 
 =item B<--delete>
 
-Delete: Delete mode.
+delete: Delete the track instead of building it.
 
 =item B<--build_region_track_only>
 
-Build_region_track_only: Build region track only.
+build_region_track_only: Build region track only.
 
-=item B<--skipCompletionCheck>
+=item B<--skip_completion_check>
 
-SkipCompletionCheck: Skip completion check.
+skip_completion_check: Skip completion check.
 
-=item B<--dry_run_insertions>, B<--dry>, B<--dryRun>
+=item B<--dry_run_insertions>, B<--dry>
 
-Dry_run_insertions: Perform a dry run of insertions.
+dry_run_insertions: Perform a dry run
 
 =item B<--log_dir>
 
-Log_dir: Directory for log files.
+log_dir: Directory for log files.
 
 =item B<--threads>
 
-Threads: Number of threads to use.
-
-=item B<--meta_only>
-
-Meta_only: Meta only mode.
+threads: Number of threads to use.
 
 =back
 
