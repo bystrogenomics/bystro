2eeeb3c36bbf3f5e0d20b6c9a1778ecc6fff11db
Lobo <austin.talbot1993@gmail.com>
2024-09-11 20:52:18 -0400

    Added randomization to adversarial method

--- python/python/bystro/supervised_ppca/ppca_augmented.py
+++ python/python/bystro/supervised_ppca/ppca_augmented.py
@@ -5,6 +5,8 @@ import numpy.linalg as la
 from bystro.supervised_ppca._base import BaseGaussianFactorModel
 from numpy.typing import NDArray
 
+from sklearn.utils.extmath import randomized_range_finder
+
 from bystro.covariance._covariance_np import (
     EmpiricalCovariance,
     LinearShrinkageCovariance,
@@ -666,6 +668,175 @@ def _select_covariance_estimator(regularization):
     else:
         raise ValueError("Unrecognized regularization %s" % regularization)
     return model_cov
+
+
+class PPCAadversarialRandomized(BaseGaussianFactorModel):
+    def __init__(
+        self,
+        n_components: int = 2,
+        mu: float = 1.0,
+        eps: float = 1e-8,
+        n_oversamples: int = 20,
+        random_state: int = 2021,
+    ):
+        super().__init__(n_components=n_components)
+        self.mu: float = mu
+        self.eps: float = eps
+        self.W_: NDArray[np.float_] | None = None
+        self.D_: NDArray[np.float_] | None = None
+        self.p: int | None = None
+        self.n_oversamples: int = n_oversamples
+        self.random_state: int = random_state
+        self.sigma2_: np.float_ | None = None
+
+    def fit(
+        self, X: NDArray[np.float_], Y: NDArray[np.float_]
+    ) -> "PPCAadversarialRandomized":
+        """
+        Fits a model given covariates X
+
+        Parameters
+        ----------
+        X : NDArray,(n_samples,n_covariates)
+            The data
+
+        Y : NDArray,(n_samples,n_confounders)
+            The concommitant data we want to remove
+
+        Returns
+        -------
+        self : PPCAadversarial
+            The model
+        """
+        self.mean_x = np.mean(X, axis=0)
+        self.mean_y = np.mean(Y, axis=0)
+        X_dm = X - self.mean_x
+        Y_dm = Y - self.mean_y
+        N, self.p = X.shape
+        q = Y.shape[1]
+
+        n_random = self.n_components + self.n_oversamples
+        Q = randomized_range_finder(
+            X_dm.T,
+            n_iter=7,
+            size=n_random,
+            power_iteration_normalizer="auto",
+            random_state=self.random_state,
+        )
+
+        X_tilde = Q.T @ X_dm.T
+
+        B_11 = np.cov(X_tilde)
+        B_12 = 1 / N * np.dot(X_tilde, Y_dm)
+        B_22 = B_12.T @ la.solve(B_11 + self.eps * np.eye(n_random), B_12)
+
+        B = np.zeros((n_random + q, n_random + q))
+        B[:n_random, :n_random] = B_11
+        B[:n_random, n_random:] = B_12
+        B[n_random:, :n_random] = -self.mu * B_12.T
+        B[n_random:, n_random:] = -self.mu * B_22
+        B = B.T
+
+        eigvals, eigvecs = la.eig(B)
+        eigvals = np.real(eigvals)
+        eigvecs = np.real(eigvecs)
+        idx = eigvals.argsort()[::-1]
+
+        self.eigvals_ = eigvals[idx]
+        V_latent = eigvecs[:, idx]
+
+        self.V_latent = V_latent
+
+        W_latent = V_latent[:n_random, : self.n_components]
+
+        W = Q @ W_latent
+
+        A = W.T @ W
+        B = W.T @ X_dm.T
+        S = la.solve(A, B).T
+        X_recon = S @ W.T
+        var = np.mean((X - X_recon) ** 2)
+        self._store_instance_variables((W, var))
+
+        return self
+
+    def get_covariance(self) -> NDArray[np.float_]:
+        """
+        Gets the covariance matrix
+
+        Sigma = W^TW + sigma2*I
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        covariance : NDArray,(p,p)
+            The covariance matrix
+        """
+        if self.W_ is None or self.sigma2_ is None or self.p is None:
+            raise ValueError("Model has not been fit yet")
+
+        return np.dot(self.W_.T, self.W_) + self.sigma2_ * np.eye(self.p)
+
+    def get_noise(self):
+        """
+        Returns the observational noise as a diagonal matrix
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        Lambda : NDArray,(p,p)
+            The observational noise
+        """
+        if self.sigma2_ is None or self.p is None:
+            raise ValueError("Model has not been fit yet")
+
+        return self.sigma2_ * np.eye(self.p)
+
+    def _store_instance_variables(
+        self,
+        trainable_variables: tuple[
+            NDArray[np.float_], np.float_, 
+        ],
+    ) -> None:
+        """
+        Saves the learned variables
+
+        Parameters
+        ----------
+        trainable_variables : list
+            List of variables saved
+
+        Sets
+        ----
+        W_ : NDArray,(n_components,p)
+            The loadings
+
+        sigma2_ : float
+            The isotropic variance
+        """
+        self.W_ = trainable_variables[0].T
+        self.sigma2_ = trainable_variables[1]
+
+    def _initialize_save_losses(self):
+        pass
+
+    def _save_losses(self):
+        pass
+
+    def _test_inputs(self):
+        pass
+
+    def _transform_training_data(self):
+        pass
+
+    def _save_variables(self):
+        pass
 
 
 """
