0fadb8912f8072c8dc06fcfcd369959473e8e8b3
Alex Kotlar <akotlar@bu.edu>
2024-09-14 17:25:32 -0400

    run perltidy on connection.pm

--- perl/lib/Utils/SqlWriter/Connection.pm
+++ perl/lib/Utils/SqlWriter/Connection.pm
@@ -5,7 +5,7 @@ use warnings;
 package Utils::SqlWriter::Connection;
 
 use DBI;
-use DBD::MariaDB 1.23;  # Specify the version of DBD::MariaDB
+use DBD::MariaDB 1.23;    # Specify the version of DBD::MariaDB
 
 our $VERSION = '0.001';
 
@@ -15,8 +15,10 @@ use Mouse 2;
 use namespace::autoclean;
 
 # The actual configuration
-has driver   => ( is => 'ro', isa => 'Str', default => "DBI:MariaDB" );  # Use MariaDB driver
-has host     => ( is => 'ro', isa => 'Str', default => "genome-mysql.soe.ucsc.edu" );
+has driver => ( is => 'ro', isa => 'Str', default => "DBI:MariaDB" )
+  ;                       # Use MariaDB driver
+has host =>
+  ( is => 'ro', isa => 'Str', default => "genome-mysql.soe.ucsc.edu" );
 has user     => ( is => 'ro', isa => 'Str', default => "genome" );
 has password => ( is => 'ro', isa => 'Str', );
 has port     => ( is => 'ro', isa => 'Int', );
@@ -24,39 +26,40 @@ has socket   => ( is => 'ro', isa => 'Str', );
 has database => ( is => 'ro', isa => 'Maybe[Str]' );
 
 around BUILDARGS => sub {
-  my ( $orig, $self, $data ) = @_;
+    my ( $orig, $self, $data ) = @_;
 
-  if ( defined $data->{connection} ) {
-    for my $key ( keys %{ $data->{connection} } ) {
-      $data->{$key} = $data->{connection}{$key};
+    if ( defined $data->{connection} ) {
+        for my $key ( keys %{ $data->{connection} } ) {
+            $data->{$key} = $data->{connection}{$key};
+        }
     }
-  }
 
-  return $self->$orig($data);
+    return $self->$orig($data);
 };
 
 sub connect {
-  my $self         = shift;
-  my $databaseName = shift;
+    my $self         = shift;
+    my $databaseName = shift;
 
-  $databaseName = $self->database || $databaseName;
+    $databaseName = $self->database || $databaseName;
 
-  my $connection = $self->driver;
-  $connection .= ":database=$databaseName;host=" . $self->host if $self->host;
-  $connection .= ";port=" . $self->port                        if $self->port;
-  $connection .= ";mysql_socket=" . $self->port_num            if $self->socket;
-  $connection .= ";mariadb_read_default_group=client";  # Change to MariaDB option
+    my $connection = $self->driver;
+    $connection .= ":database=$databaseName;host=" . $self->host if $self->host;
+    $connection .= ";port=" . $self->port                        if $self->port;
+    $connection .= ";mysql_socket=" . $self->port_num if $self->socket;
+    $connection .=
+      ";mariadb_read_default_group=client";    # Change to MariaDB option
 
-  return DBI->connect(
-    $connection,
-    $self->user,
-    $self->password,
-    {
-      RaiseError => 1,
-      PrintError => 1,
-      AutoCommit => 1
-    }
-  );
+    return DBI->connect(
+        $connection,
+        $self->user,
+        $self->password,
+        {
+            RaiseError => 1,
+            PrintError => 1,
+            AutoCommit => 1
+        }
+    );
 }
 
 __PACKAGE__->meta->make_immutable();
