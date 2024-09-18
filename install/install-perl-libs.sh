96de231a7975aeebeda8d0e422c7c75d724876f4
Alex Kotlar <akotlar@bu.edu>
2024-09-18 13:24:49 -0400

    attempt to rely on cpanfile to simplify dependencies

--- install/install-perl-libs.sh
+++ install/install-perl-libs.sh
@@ -5,18 +5,6 @@ echo -e "\n\nInstalling Perl libraries\n"
 
 echo "PERL ROOT IN install/install-perl-libs.sh: $PERLBREW_ROOT"
 
-# Install multiple modules at once using cpm
-cpm install -g --test Capture::Tiny Mouse Path::Tiny namespace::autoclean DDP YAML::XS JSON::XS Getopt::Long::Descriptive Types::Path::Tiny Sereal MCE::Shared List::MoreUtils Log::Fast Parallel::ForkManager Cpanel::JSON::XS Mouse::Meta::Attribute::Custom::Trait::Array Net::HTTP Math::SigFigs PerlIO::utf8_strict PerlIO::gzip MouseX::SimpleConfig MouseX::ConfigFromFile Archive::Extract DBI String::Strip File::Which Hash::Merge::Simple Module::Build::XSUtil Test::LeakTrace Test::Pod Test::Exception Log::Any::Adapter File::Copy::Recursive
-
-# Install modules that require special handling
-ALIEN_INSTALL_TYPE=share cpm install -g --test Alien::LMDB
-cpm install -g --test LMDB_File
-cpm install -g MouseX::Getopt # fails due to differences from expected output; unimportant
-cpm install -g --test IO::FDPass
-cpm install -g --test Beanstalk::Client
-cpm install -g --test Sys::CpuAffinity
-cpm install -g --test DBD::MariaDB
-
 # Custom installation of Data::MessagePack
 install_custom_msgpack() {
     rm -rf msgpack-perl
@@ -28,3 +16,11 @@ install_custom_msgpack() {
 }
 
 install_custom_msgpack
+
+ALIEN_INSTALL_TYPE=share cpm install -g --test Alien::LMDB
+cpm install -g --test LMDB_File
+
+# no --test option because it has a trivial failure related to formatting of cli help strings
+cpm install -g MouseX::Getopt
+
+cpm install -g --test --cpanfile ../perl/cpanfile
