693cca40512914a6ad862f23191d29e2ab02a401
Alex Kotlar <akotlar@bu.edu>
2024-10-02 20:50:56 -0400

    fix

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,23 @@
-from setuptools import setup # type: ignore
-from Cython.Build import cythonize # type: ignore
+from setuptools import setup
+from Cython.Build import cythonize
+import glob
+import os
+
+# Search for .pyx files in the specified directories
+pyx_files = glob.glob("python/bystro/**/**/*.pyx", recursive=True)
+
+# Only cythonize if .pyx files are found
+if pyx_files:
+    ext_modules = cythonize(
+        pyx_files,
+        build_dir="build",
+        compiler_directives={"language_level": "3"},
+    )
+else:
+    ext_modules = []  # No extensions if no .pyx files are found
 
 setup(
     name="bystro",
-    package_dir={"bystro": "python/bystro"},
-    ext_modules=cythonize(
-        "python/bystro/**/**/*.pyx",
-        build_dir="build",
-        compiler_directives={"language_level": "3"},
-    ),
-    zip_safe=False
+    ext_modules=ext_modules,
+    # Other setup parameters here
 )
