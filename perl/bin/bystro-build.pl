62257bc1a409075b6502af51e0b9159ef7410d5c
Alex V. Kotlar <akotlar@bu.edu>
2024-10-03 14:04:18 -0400

    Feature/better bystro build (#582)
    * Improved bystro-build.pl cli interface
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    	- Streamlined command-line interface for improved usability.
    	- Enhanced error messaging for the required `--config` option.
    - **Documentation**
    - Updated documentation to reflect changes in command-line options and
    structure.
    	- Changed author attribution from "Thomas Wingo" to "Bystro Team."
    	- Improved clarity of debug output in documentation.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- perl/bin/bystro-build.pl
+++ perl/bin/bystro-build.pl
@@ -16,62 +16,60 @@ use DDP;
 use Seq::Build;
 
 my (
-  $yaml_config, $wantedType,      $wantedName, $verbose,
-  $maxThreads,  $help,            $wantedChr,  $dryRunInsertions,
-  $logDir,      $metaOnly,        $debug,      $overwrite,
-  $delete,      $regionTrackOnly, $skipCompletionCheck
+  $yaml_config,     $wantedType, $wantedName, $verbose,
+  $maxThreads,      $help,       $wantedChr,  $dryRunInsertions,
+  $logDir,          $debug,      $overwrite,  $delete,
+  $regionTrackOnly, $skipCompletionCheck
 );
 
 $debug = 0;
+
 # usage
 GetOptions(
-  'c|config=s'                                => \$yaml_config,
-  't|type|wantedType=s'                       => \$wantedType,
-  'n|name|wantedName=s'                       => \$wantedName,
-  'v|verbose=i'                               => \$verbose,
-  'h|help'                                    => \$help,
-  'd|debug=i'                                 => \$debug,
-  'o|overwrite'                               => \$overwrite,
-  'chr|wantedChr=s'                           => \$wantedChr,
-  'delete'                                    => \$delete,
-  'build_region_track_only'                   => \$regionTrackOnly,
-  'skipCompletionCheck|skip_completion_check' => \$skipCompletionCheck,
-  'dry_run_insertions|dry|dryRun'             => \$dryRunInsertions,
-  'log_dir=s'                                 => \$logDir,
-  'threads=i'                                 => \$maxThreads,
-  'meta_only'                                 => \$metaOnly,
-);
+  'c|config=s'                    => \$yaml_config,
+  't|type=s'                      => \$wantedType,
+  'n|name=s'                      => \$wantedName,
+  'v|verbose=i'                   => \$verbose,
+  'h|help'                        => \$help,
+  'd|debug=i'                     => \$debug,
+  'o|overwrite'                   => \$overwrite,
+  'chr=s'                         => \$wantedChr,
+  'delete'                        => \$delete,
+  'build_region_track_only'       => \$regionTrackOnly,
+  'skip_completion_check'         => \$skipCompletionCheck,
+  'dry_run_insertions|dry|dryRun' => \$dryRunInsertions,
+  'log_dir=s'                     => \$logDir,
+  'threads=i'                     => \$maxThreads
+) or pod2usage(2);
 
 if ($help) {
-  Pod::Usage::pod2usage(1);
+  pod2usage(1);
   exit;
 }
 
-my %options = (
-  'c|config=s'                                => \$yaml_config,
-  't|type|wantedType=s'                       => \$wantedType,
-  'n|name|wantedName=s'                       => \$wantedName,
-  'v|verbose=i'                               => \$verbose,
-  'h|help'                                    => \$help,
-  'd|debug=i'                                 => \$debug,
-  'o|overwrite'                               => \$overwrite,
-  'chr|wantedChr=s'                           => \$wantedChr,
-  'delete'                                    => \$delete,
-  'build_region_track_only'                   => \$regionTrackOnly,
-  'skipCompletionCheck|skip_completion_check' => \$skipCompletionCheck,
-  'dry_run_insertions|dry|dryRun'             => \$dryRunInsertions,
-  'log_dir=s'                                 => \$logDir,
-  'maxThreads=i'                              => \$maxThreads,
-  'meta_only'                                 => \$metaOnly,
-);
-
 unless ($yaml_config) {
-  Pod::Usage::pod2usage();
+  pod2usage("Error: --config is required");
 }
 
 if ($debug) {
   say STDERR "Running with the following parameters:";
-  p %options;
+  my $options = {
+    config                  => $yaml_config,
+    wantedChr               => $wantedChr,
+    wantedType              => $wantedType,
+    wantedName              => $wantedName,
+    overwrite               => $overwrite || 0,
+    debug                   => $debug     || 0,
+    delete                  => !!$delete,
+    build_region_track_only => !!$regionTrackOnly,
+    skipCompletionCheck     => !!$skipCompletionCheck,
+    dryRunInsertions        => !!$dryRunInsertions,
+    logDir                  => $logDir,
+    maxThreads              => $maxThreads,
+    verbose                 => $verbose,
+  };
+
+  p $options;
 }
 
 # read config file to determine genome name for log and check validity
@@ -110,67 +108,113 @@ my $builder_options_href = {
   build_region_track_only => !!$regionTrackOnly,
   skipCompletionCheck     => !!$skipCompletionCheck,
   dryRun                  => !!$dryRunInsertions,
-  meta_only               => !!$metaOnly,
   verbose                 => $verbose,
 };
 
 if ( defined $maxThreads ) {
   $builder_options_href->{maxThreads} = $maxThreads;
 }
-# my $log_file = path(".")->child($log_name)->absolute->stringify;
-# Log::Any::Adapter->set( 'File', $log_file );
 
 my $builder = Seq::Build->new_with_config($builder_options_href);
-
-#say "done: " . $wantedType || $wantedName . $wantedChr ? ' for $wantedChr' : '';
 
 __END__
 
 =head1 NAME
 
-build_genome_assembly - builds a binary genome assembly
+build_genome_assembly - Builds a binary genome assembly
 
 =head1 SYNOPSIS
 
-build_genome_assembly
-  --config <file>
-  --type <'genome', 'conserv', 'transcript_db', 'snp_db', 'gene_db'>
-  [ --wanted_chr ]
+build_genome_assembly [options]
+
+ Options:
+   -c, --config                   YAML configuration file
+   -t, --type                     Type of build (e.g., genome, conserv, transcript_db, gene_db, snp_db)
+   -n, --name                     Name of the build
+   -v, --verbose                  Verbosity level
+   -h, --help                     Display this help message
+   -d, --debug                    Debug level (default: 0)
+   -o, --overwrite                For a given track, overwrite existing track values, rather than merging them
+   --chr                          Chromosome to build (if applicable)
+   --delete                       Delete the track instead of building it
+   --build_region_track_only      Build region track only
+   --skip_completion_check        Skip completion check
+   --dry_run_insertions, --dry    Perform a dry run
+   --log_dir                      Directory for log files
+   --threads                      Number of threads to use
 
 =head1 DESCRIPTION
 
-C<build_genome_assembly.pl> takes a yaml configuration file and reads raw genomic
+C<build_genome_assembly.pl> takes a YAML configuration file and reads raw genomic
 data that has been previously downloaded into the 'raw' folder to create the binary
-index of the genome and assocated annotations in the mongodb instance.
+index of the genome and associated annotations in the MongoDB instance.
 
 =head1 OPTIONS
 
 =over 8
 
-=item B<-t>, B<--type>
-
-Type: A general command to start building; genome, conserv, transcript_db, gene_db
-or snp_db.
-
 =item B<-c>, B<--config>
 
-Config: A YAML genome assembly configuration file that specifies the various
+config: A YAML genome assembly configuration file that specifies the various
 tracks and data associated with the assembly. This is the same file that is
-used by the Seq Package to annotate snpfiles.
+used by the Bystro Package to annotate VCF and SNP files.
 
-=item B<-w>, B<--wanted_chr>
+=item B<-t>, B<--type>
 
-Wanted_chr: chromosome to build, if building gene or snp; will build all if not
-specified.
+type: Build all tracks in the configuration file with `type: <this_type>`.
+
+=item B<-n>, B<--name>
+
+name: Build the track specified in the configuration file with `name: <this_name>`.
+
+=item B<--chr>
+
+chr: Chromosome to build, if building gene or SNP; will build all if not specified.
+
+=item B<-v>, B<--verbose>
+
+verbose: Verbosity level (default: 0).
+
+=item B<-d>, B<--debug>
+
+debug: Debug level (default: 0).
+
+=item B<-o>, B<--overwrite>
+
+overwrite: For a given track, overwrite existing track values, rather than merging them
+
+=item B<--delete>
+
+delete: Delete the track instead of building it.
+
+=item B<--build_region_track_only>
+
+build_region_track_only: Build region track only.
+
+=item B<--skip_completion_check>
+
+skip_completion_check: Skip completion check.
+
+=item B<--dry_run_insertions>, B<--dry>
+
+dry_run_insertions: Perform a dry run
+
+=item B<--log_dir>
+
+log_dir: Directory for log files.
+
+=item B<--threads>
+
+threads: Number of threads to use.
 
 =back
 
 =head1 AUTHOR
 
-Thomas Wingo
+Bystro Team
 
 =head1 SEE ALSO
 
-Seq Package
+Bystro Package
 
 =cut
